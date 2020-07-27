34d8c476789fdfa4e9a33b0a5e60a3dece59fcd9
Jonathan Müller <git@foonathan.net>
2020-07-27 21:57:55 +0200

    Add explicit test for atom as pattern

---
+++ test/pattern/atom.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2020 Jonathan Müller <jonathanmueller.dev@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#include <lexy/dsl/base.hpp>
+
+#include "verify.hpp"
+
+TEST_CASE("pattern: atom")
+{
+    constexpr auto pattern = LEXY_LIT("abc");
+    CHECK(lexy::is_pattern<decltype(pattern)>);
+
+    constexpr auto empty = pattern_matches(pattern, "");
+    CHECK(!empty);
+
+    constexpr auto success = pattern_matches(pattern, "abc");
+    CHECK(success);
+    CHECK(success.id() == 0);
+    CHECK(success.match().string_view() == "abc");
+}
+
