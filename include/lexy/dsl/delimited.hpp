4040cba724487cc3cbe582a735ec01e898cd4ca2
Jonathan Müller <git@foonathan.net>
2020-09-23 16:26:54 +0200

    Add escape sequence support to delimited

--- include/lexy/dsl/delimited.hpp
+++ include/lexy/dsl/delimited.hpp
@@ -34,8 +34,58 @@ struct missing_delimiter
 
 namespace lexyd
 {
+template <typename Escape, typename CodePoint, typename Close>
+struct _delim : rule_base
+{
+    static constexpr auto has_matcher = false;
+
+    template <typename NextParser>
+    struct parser
+    {
+        template <typename Context, typename Input, typename... Args>
+        LEXY_DSL_FUNC auto parse(Context& context, Input& input, Args&&... args) ->
+            typename Context::result_type
+        {
+            auto sink = context.list_sink();
+
+            const auto begin = input.cur();
+            while (true)
+            {
+                if (input.peek() == Input::encoding::eof())
+                    // We're missing the final delimiter.
+                    return LEXY_MOV(context).error(input,
+                                                   lexy::missing_delimiter::error<Input>(begin));
+                else if (auto pos = input.cur(); Escape::escape_matcher::match(input))
+                {
+                    // We have an escape character.
+                    if (!Escape::match_arg(sink, input))
+                        // Invalid escape sequence.
+                        return Escape::report_error(context, input, pos);
+                }
+                else if (Close::matcher::match(input))
+                    // Done with the string.
+                    break;
+                else
+                {
+                    // Match a code point.
+                    if (auto pos = input.cur(); CodePoint::match(input))
+                    {
+                        for (auto end = input.cur(); pos != end; ++pos)
+                            sink(*pos);
+                    }
+                    else
+                        return CodePoint::report_error(context, input, pos);
+                }
+            }
+
+            // Add the final string as an argument.
+            return NextParser::parse(context, input, LEXY_FWD(args)..., LEXY_MOV(sink).finish());
+        }
+    };
+};
+
 template <typename CodePoint, typename Close>
-struct _delim_end : rule_base
+struct _delim<void, CodePoint, Close> : rule_base
 {
     static constexpr auto has_matcher = false;
 
@@ -48,32 +98,35 @@ struct _delim_end : rule_base
         {
             const auto begin = input.cur();
 
-            auto end = input.cur();
-            while (!Close::matcher::match(input))
+            auto pos = input.cur();
+            while (true)
             {
                 if (input.peek() == Input::encoding::eof())
                     // We're missing the final delimiter.
                     return LEXY_MOV(context).error(input,
                                                    lexy::missing_delimiter::error<Input>(begin));
+                else if (Close::matcher::match(input))
+                    // Done with the string.
+                    break;
+                else
+                {
+                    // Match a code point.
+                    if (!CodePoint::match(input))
+                        return CodePoint::report_error(context, input, pos);
 
-                // We didn't match the closing delimiter.
-                // Advance to the next code point and increase content length.
-                if (!CodePoint::match(input))
-                    // We know that end was the previous position.
-                    return CodePoint::report_error(context, input, end);
-
-                end = input.cur();
+                    pos = input.cur();
+                }
             }
 
             // Add the lexeme as an argument.
             return NextParser::parse(context, input, LEXY_FWD(args)...,
-                                     lexy::lexeme<Input>(begin, end));
+                                     lexy::lexeme<Input>(begin, pos));
         }
     };
 };
 
 template <typename Open, typename Close, typename Whitespace>
-struct _delim
+struct _delim_dsl
 {
     /// Specifies the atomic for a valid code point.
     template <typename CodePoint>
@@ -81,9 +134,20 @@ struct _delim
     {
         static_assert(lexy::is_atom<CodePoint>);
         if constexpr (std::is_same_v<Whitespace, void>)
-            return Open{} >> _delim_end<CodePoint, Close>{};
+            return Open{} >> _delim<void, CodePoint, Close>{};
         else
-            return Whitespace{} + Open{} >> _delim_end<CodePoint, Close>{};
+            return Whitespace{} + Open{} >> _delim<void, CodePoint, Close>{};
+    }
+
+    /// Specifies the atomic for a valid code point and an escape sequence.
+    template <typename CodePoint, typename Escape>
+    LEXY_CONSTEVAL auto operator()(CodePoint, Escape) const
+    {
+        static_assert(lexy::is_atom<CodePoint>);
+        if constexpr (std::is_same_v<Whitespace, void>)
+            return Open{} >> _delim<Escape, CodePoint, Close>{};
+        else
+            return Whitespace{} + Open{} >> _delim<Escape, CodePoint, Close>{};
     }
 
     /// Sets the whitespace pattern.
@@ -92,7 +156,7 @@ struct _delim
     LEXY_CONSTEVAL auto operator[](Ws) const
     {
         static_assert(lexy::is_pattern<Ws>, "whitespace must be a pattern");
-        return _delim<Open, Close, Ws>{};
+        return _delim_dsl<Open, Close, Ws>{};
     }
 };
 
@@ -101,7 +165,7 @@ template <typename Open, typename Close>
 LEXY_CONSTEVAL auto delimited(Open, Close)
 {
     static_assert(lexy::is_pattern<Open> && lexy::is_pattern<Close>);
-    return _delim<Open, Close, void>{};
+    return _delim_dsl<Open, Close, void>{};
 }
 
 /// Parses everything between a paired delimiter.
@@ -109,7 +173,7 @@ template <typename Delim>
 LEXY_CONSTEVAL auto delimited(Delim)
 {
     static_assert(lexy::is_pattern<Delim>);
-    return _delim<Delim, Delim, void>{};
+    return _delim_dsl<Delim, Delim, void>{};
 }
 
 constexpr auto quoted        = delimited(LEXY_LIT("\""));
@@ -120,6 +184,97 @@ constexpr auto single_quoted = delimited(LEXY_LIT("'"));
 constexpr auto backticked        = delimited(LEXY_LIT("`"));
 constexpr auto double_backticked = delimited(LEXY_LIT("``"));
 constexpr auto triple_backticked = delimited(LEXY_LIT("```"));
+} // namespace lexyd
+
+namespace lexy
+{
+struct invalid_escape_sequence
+{
+    template <typename Input>
+    class error
+    {
+    public:
+        constexpr explicit error(typename Input::iterator pos) noexcept : _pos(pos) {}
+
+        constexpr auto position() const noexcept
+        {
+            return _pos;
+        }
+
+    private:
+        typename Input::iterator _pos;
+    };
+};
+} // namespace lexy
+
+namespace lexyd
+{
+template <typename Pattern, typename Replacement>
+struct _escape_lit
+{
+    template <typename Sink, typename Input>
+    LEXY_DSL_FUNC bool try_match(Sink& sink, Input& input)
+    {
+        if (!Pattern::matcher::match(input))
+            return false;
+
+        for (auto c : Replacement::get())
+            sink(c);
+        return true;
+    }
+};
+
+template <typename EscapePattern, typename... EscapeArguments>
+struct _escape
+{
+    using escape_matcher = typename EscapePattern::matcher;
+
+    template <typename Sink, typename Input>
+    LEXY_DSL_FUNC bool match_arg(Sink& sink, Input& input)
+    {
+        return (EscapeArguments::try_match(sink, input) || ...);
+    }
+
+    template <typename Context, typename Input>
+    LEXY_DSL_FUNC auto report_error(Context& context, Input& input, typename Input::iterator pos) ->
+        typename Context::result_type
+    {
+        return LEXY_MOV(context).error(input, lexy::invalid_escape_sequence::error<Input>(pos));
+    }
+
+    //=== dsl ===//
+    /// Replace the escaped pattern with the literal string value.
+    template <typename Pattern, typename Replacement>
+    LEXY_CONSTEVAL auto literal(Pattern, Replacement) const
+    {
+        static_assert(lexy::is_pattern<Pattern>);
+        return _escape<EscapePattern, EscapeArguments..., _escape_lit<Pattern, Replacement>>{};
+    }
+    /// Replace the escaped literal with itself.
+    template <typename Replacement>
+    LEXY_CONSTEVAL auto literal(Replacement rep) const
+    {
+        return literal(_lit<Replacement>{}, rep);
+    }
+};
+
+template <typename EscapePattern>
+LEXY_CONSTEVAL auto escape(EscapePattern)
+{
+    static_assert(lexy::is_pattern<EscapePattern>);
+    return _escape<EscapePattern>{};
+}
+
+#if LEXY_HAS_NTTP
+/// Defines the replacement string of a literal replacement.
+template <lexy::_detail::string_literal Str>
+constexpr auto escape_value = lexy::_detail::type_string<Str>;
+#endif
+
+#define LEXY_ESCAPE_VALUE(Str)                                                                     \
+    LEXY_NTTP_STRING(Str) {}
+
+constexpr auto backslash_escape = escape(LEXY_LIT("\\"));
 } // namespace lexyd
 
 #endif // LEXY_DSL_DELIMITED_HPP_INCLUDED
--- test/rule/delimited.cpp
+++ test/rule/delimited.cpp
@@ -121,6 +121,91 @@ TEST_CASE("rule: delimited")
         constexpr auto invalid_ascii = rule_matches<callback>(rule, " (ab\xFF");
         CHECK(invalid_ascii == -3);
     }
+    SUBCASE("escape")
+    {
+        constexpr auto escape
+            = lexy::dsl::backslash_escape.literal(LEXY_LIT(")"), LEXY_ESCAPE_VALUE("))"));
+        constexpr auto rule = delimited(LEXY_LIT("("), LEXY_LIT(")"))(cp, escape);
+        CHECK(lexy::is_rule<decltype(rule)>);
+        CHECK(!lexy::is_pattern<decltype(rule)>);
+        CHECK(lexy::is_branch_rule<decltype(rule)>);
+
+        struct callback
+        {
+            const char* str;
+
+            constexpr auto list()
+            {
+                struct b
+                {
+                    int count = 0;
+
+                    using return_type = int;
+
+                    constexpr void operator()(char)
+                    {
+                        ++count;
+                    }
+
+                    constexpr int finish() &&
+                    {
+                        return count;
+                    }
+                };
+                return b{};
+            }
+            constexpr int success(const char* cur, int count)
+            {
+                assert(cur - str == count + 2);
+                return count;
+            }
+
+            constexpr int error(test_error<lexy::expected_literal> e)
+            {
+                assert(e.string() == "(");
+                assert(e.position() == str);
+                return -1;
+            }
+            constexpr int error(test_error<lexy::missing_delimiter> e)
+            {
+                assert(e.position() == str + 1);
+                return -2;
+            }
+            constexpr int error(test_error<lexy::expected_char_class> e)
+            {
+                assert(e.character_class() == lexy::_detail::string_view("ASCII"));
+                return -3;
+            }
+            constexpr int error(test_error<lexy::invalid_escape_sequence>)
+            {
+                return -4;
+            }
+        };
+
+        constexpr auto empty = rule_matches<callback>(rule, "");
+        CHECK(empty == -1);
+
+        constexpr auto zero = rule_matches<callback>(rule, "()");
+        CHECK(zero == 0);
+        constexpr auto one = rule_matches<callback>(rule, "(a)");
+        CHECK(one == 1);
+        constexpr auto two = rule_matches<callback>(rule, "(ab)");
+        CHECK(two == 2);
+        constexpr auto three = rule_matches<callback>(rule, "(abc)");
+        CHECK(three == 3);
+
+        constexpr auto escaped = rule_matches<callback>(rule, "(a\\)b)");
+        CHECK(escaped == 4);
+
+        constexpr auto unterminated = rule_matches<callback>(rule, "(abc");
+        CHECK(unterminated == -2);
+
+        constexpr auto invalid_ascii = rule_matches<callback>(rule, "(ab\xFF");
+        CHECK(invalid_ascii == -3);
+
+        constexpr auto invalid_escape = rule_matches<callback>(rule, "(a\\fb)");
+        CHECK(invalid_escape == -4);
+    }
     SUBCASE("predefined")
     {
         CHECK(lexy::match(lexy::zstring_input(R"("abc")"), lexy::dsl::quoted(cp) + lexy::dsl::eof));
