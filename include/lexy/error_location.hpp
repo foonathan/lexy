8d0e0e6774491903bf10b3485275488edf93a716
Jonathan Müller <git@foonathan.net>
2020-10-01 21:49:00 +0200

    Add lexy::make_error_location

---
+++ include/lexy/error_location.hpp
@@ -0,0 +1,101 @@
+// Copyright (C) 2020 Jonathan Müller <jonathanmueller.dev@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#ifndef LEXY_ERROR_LOCATION_HPP_INCLUDED
+#define LEXY_ERROR_LOCATION_HPP_INCLUDED
+
+#include <lexy/dsl/base.hpp>
+#include <lexy/input/base.hpp>
+#include <lexy/lexeme.hpp>
+
+namespace lexy
+{
+template <typename Encoding, typename Iterator>
+struct error_location
+{
+    std::size_t line, column;
+    /// The entire line where the error occurred.
+    lexeme<Encoding, Iterator> context;
+};
+
+template <typename Input, typename PatternCP, typename PatternNL>
+constexpr auto make_error_location(const Input& input, typename input_reader<Input>::iterator pos,
+                                   PatternCP, PatternNL)
+{
+    static_assert(is_pattern<PatternCP> && is_pattern<PatternNL>);
+
+    auto reader      = input.reader();
+    using encoding   = typename decltype(reader)::encoding;
+    using iterator   = typename decltype(reader)::iterator;
+    using lexeme_t   = lexeme<encoding, iterator>;
+    using location_t = error_location<encoding, iterator>;
+
+    // We start at the first line and first column.
+    std::size_t cur_line   = 1;
+    std::size_t cur_column = 1;
+    auto        line_start = reader.cur();
+
+    while (true)
+    {
+        if (reader.cur() == pos)
+        {
+            // We found the position of the error.
+            break;
+        }
+        else if (PatternNL::matcher::match(reader))
+        {
+            // We're at a new line.
+            ++cur_line;
+            cur_column = 1;
+            line_start = reader.cur();
+        }
+        else if (PatternCP::matcher::match(reader))
+        {
+            // Next column.
+            ++cur_column;
+        }
+        else if (reader.peek() == encoding::eof())
+        {
+            // We have an OOB error position, return what the current position (i.e. the end of the
+            // file).
+            return location_t{cur_line, cur_column, lexeme_t(reader, line_start)};
+        }
+        else
+        {
+            // Invalid code unit, just ignore it in the column count.
+            reader.bump();
+        }
+    }
+
+    // At this point, we've found the right line and column number and the reader is at the error
+    // position.
+    // We now need to find the end of the current line to compute context.
+    while (true)
+    {
+        auto save = reader;
+        if (PatternNL::match(reader))
+        {
+            // We've found the final newline, reset to previous position.
+            reader = LEXY_MOV(save);
+            break;
+        }
+        else if (reader.peek() == encoding::eof())
+        {
+            // Last line of the input.
+            break;
+        }
+        else
+        {
+            // Try again.
+            reader.bump();
+        }
+    }
+
+    // We've found the right line and column number and the reader is at the end of the line.
+    return location_t{cur_line, cur_column, lexeme_t(reader, line_start)};
+}
+} // namespace lexy
+
+#endif // LEXY_ERROR_LOCATION_HPP_INCLUDED
+
---
+++ test/error_location.cpp
@@ -0,0 +1,141 @@
+// Copyright (C) 2020 Jonathan Müller <jonathanmueller.dev@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#include <lexy/error_location.hpp>
+
+#include <doctest.h>
+#include <lexy/dsl/ascii.hpp>
+#include <lexy/dsl/literal.hpp>
+#include <lexy/input/string_input.hpp>
+
+TEST_CASE("make_error_location")
+{
+    constexpr auto character = lexy::dsl::ascii::character;
+    constexpr auto newline   = lexy::dsl::lit_c<'\n'>;
+
+    SUBCASE("basic")
+    {
+        constexpr auto input = lexy::zstring_input("Line 1\n"
+                                                   "Line 2\n"
+                                                   "Line 3\n");
+
+        constexpr auto first_1
+            = lexy::make_error_location(input, input.begin(), character, newline);
+        CHECK(first_1.line == 1);
+        CHECK(first_1.column == 1);
+        CHECK(first_1.context.string_view() == "Line 1");
+
+        constexpr auto first_2
+            = lexy::make_error_location(input, input.begin() + 4, character, newline);
+        CHECK(first_2.line == 1);
+        CHECK(first_2.column == 5);
+        CHECK(first_2.context.string_view() == "Line 1");
+
+        constexpr auto second
+            = lexy::make_error_location(input, input.begin() + 10, character, newline);
+        CHECK(second.line == 2);
+        CHECK(second.column == 4);
+        CHECK(second.context.string_view() == "Line 2");
+
+        constexpr auto third
+            = lexy::make_error_location(input, input.begin() + 17, character, newline);
+        CHECK(third.line == 3);
+        CHECK(third.column == 4);
+        CHECK(third.context.string_view() == "Line 3");
+
+        constexpr auto nl = lexy::make_error_location(input, input.begin() + 6, character, newline);
+        CHECK(input.begin()[6] == '\n');
+        CHECK(nl.line == 1);
+        CHECK(nl.column == 7);
+        CHECK(nl.context.string_view() == "Line 1");
+
+        constexpr auto last = lexy::make_error_location(input, input.end() - 1, character, newline);
+        CHECK(last.line == 3);
+        CHECK(last.column == 7);
+        CHECK(last.context.string_view() == "Line 3");
+
+        constexpr auto end = lexy::make_error_location(input, input.end(), character, newline);
+        CHECK(end.line == 4);
+        CHECK(end.column == 1);
+        CHECK(end.context.string_view().empty());
+
+        // +1 is ok, input is null-terminated so it is the real one past location of the array
+        constexpr auto out_of_bounds
+            = lexy::make_error_location(input, input.end() + 1, character, newline);
+        CHECK(out_of_bounds.line == 4);
+        CHECK(out_of_bounds.column == 1);
+        CHECK(out_of_bounds.context.string_view().empty());
+    }
+    SUBCASE("no trailing newline")
+    {
+        constexpr auto input = lexy::zstring_input("Line 1\n"
+                                                   "Line 2\n"
+                                                   "Line 3");
+
+        constexpr auto first_1
+            = lexy::make_error_location(input, input.begin(), character, newline);
+        CHECK(first_1.line == 1);
+        CHECK(first_1.column == 1);
+        CHECK(first_1.context.string_view() == "Line 1");
+
+        constexpr auto first_2
+            = lexy::make_error_location(input, input.begin() + 4, character, newline);
+        CHECK(first_2.line == 1);
+        CHECK(first_2.column == 5);
+        CHECK(first_2.context.string_view() == "Line 1");
+
+        constexpr auto second
+            = lexy::make_error_location(input, input.begin() + 10, character, newline);
+        CHECK(second.line == 2);
+        CHECK(second.column == 4);
+        CHECK(second.context.string_view() == "Line 2");
+
+        constexpr auto third
+            = lexy::make_error_location(input, input.begin() + 17, character, newline);
+        CHECK(third.line == 3);
+        CHECK(third.column == 4);
+        CHECK(third.context.string_view() == "Line 3");
+
+        constexpr auto nl = lexy::make_error_location(input, input.begin() + 6, character, newline);
+        CHECK(input.begin()[6] == '\n');
+        CHECK(nl.line == 1);
+        CHECK(nl.column == 7);
+        CHECK(nl.context.string_view() == "Line 1");
+
+        constexpr auto last = lexy::make_error_location(input, input.end() - 1, character, newline);
+        CHECK(last.line == 3);
+        CHECK(last.column == 6);
+        CHECK(last.context.string_view() == "Line 3");
+
+        constexpr auto end = lexy::make_error_location(input, input.end(), character, newline);
+        CHECK(end.line == 3);
+        CHECK(end.column == 7);
+        CHECK(end.context.string_view() == "Line 3");
+    }
+    SUBCASE("weird character")
+    {
+        // 0xFF isn't an ascii character
+        static constexpr char array[] = {'a', 'b', 'c', char(0xFF), 'd', '\0'};
+        constexpr auto        input   = lexy::zstring_input(array);
+
+        constexpr auto before
+            = lexy::make_error_location(input, input.begin() + 2, character, newline);
+        CHECK(before.line == 1);
+        CHECK(before.column == 3);
+        CHECK(before.context.string_view().size() == 5);
+
+        constexpr auto weird
+            = lexy::make_error_location(input, input.begin() + 3, character, newline);
+        CHECK(weird.line == 1);
+        CHECK(weird.column == 4);
+        CHECK(weird.context.string_view().size() == 5);
+
+        constexpr auto after
+            = lexy::make_error_location(input, input.begin() + 4, character, newline);
+        CHECK(after.line == 1);
+        CHECK(after.column == 4);
+        CHECK(after.context.string_view().size() == 5);
+    }
+}
+
