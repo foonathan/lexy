3c0a7306b84311f4d5830b7f0833468e75b02ac6
Jonathan Müller <git@foonathan.net>
2020-10-02 17:00:43 +0200

    Add umbrella DSL header

---
+++ include/lexy/dsl.hpp
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 Jonathan Müller <jonathanmueller.dev@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#ifndef LEXY_DSL_HPP_INCLUDED
+#define LEXY_DSL_HPP_INCLUDED
+
+#include <lexy/dsl/alternative.hpp>
+#include <lexy/dsl/any.hpp>
+#include <lexy/dsl/ascii.hpp>
+#include <lexy/dsl/brackets.hpp>
+#include <lexy/dsl/branch.hpp>
+#include <lexy/dsl/capture.hpp>
+#include <lexy/dsl/choice.hpp>
+#include <lexy/dsl/condition.hpp>
+#include <lexy/dsl/delimited.hpp>
+#include <lexy/dsl/digit.hpp>
+#include <lexy/dsl/eof.hpp>
+#include <lexy/dsl/error.hpp>
+#include <lexy/dsl/integer.hpp>
+#include <lexy/dsl/label.hpp>
+#include <lexy/dsl/list.hpp>
+#include <lexy/dsl/literal.hpp>
+#include <lexy/dsl/newline.hpp>
+#include <lexy/dsl/option.hpp>
+#include <lexy/dsl/position.hpp>
+#include <lexy/dsl/production.hpp>
+#include <lexy/dsl/punctuator.hpp>
+#include <lexy/dsl/sequence.hpp>
+#include <lexy/dsl/sign.hpp>
+#include <lexy/dsl/success.hpp>
+#include <lexy/dsl/try.hpp>
+#include <lexy/dsl/value.hpp>
+#include <lexy/dsl/while.hpp>
+#include <lexy/dsl/whitespace.hpp>
+
+#endif // LEXY_DSL_HPP_INCLUDED
+
