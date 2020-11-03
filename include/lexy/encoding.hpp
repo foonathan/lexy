4996fce8b785630973e2a8bf82415ab5063cccaa
Jonathan Müller <git@foonathan.net>
2020-11-03 11:44:37 +0100

    Add code point decoding facilities

--- include/lexy/encoding.hpp
+++ include/lexy/encoding.hpp
@@ -16,7 +16,8 @@ namespace lexy
 class code_point
 {
 public:
-    constexpr explicit code_point(std::uint_least32_t value) noexcept : _value(value) {}
+    constexpr code_point() noexcept : _value(0xFFFF'FFFF) {}
+    constexpr explicit code_point(char32_t value) noexcept : _value(value) {}
 
     constexpr auto value() const noexcept
     {
@@ -43,8 +44,17 @@ public:
         return _value <= 0xFFFF;
     }
 
+    friend constexpr bool operator==(code_point lhs, code_point rhs) noexcept
+    {
+        return lhs._value == rhs._value;
+    }
+    friend constexpr bool operator!=(code_point lhs, code_point rhs) noexcept
+    {
+        return lhs._value != rhs._value;
+    }
+
 private:
-    std::uint_least32_t _value;
+    char32_t _value;
 };
 } // namespace lexy
 
@@ -96,9 +106,35 @@ struct default_encoding
         LEXY_PRECONDITION(cp.is_ascii());
         LEXY_PRECONDITION(size >= 1);
 
-        *buffer = static_cast<char_type>(cp.value());
+        *buffer = char_type(cp.value());
         return 1;
     }
+
+    class code_point_decoder
+    {
+    public:
+        int init(int_type c)
+        {
+            if (c == eof())
+                return -1;
+
+            _result = char32_t(c);
+            return 0;
+        }
+
+        bool next(int_type)
+        {
+            return false;
+        }
+
+        auto finish() &&
+        {
+            return code_point(_result);
+        }
+
+    private:
+        char32_t _result = {};
+    };
 };
 
 // An encoding where the input is assumed to be valid ASCII.
@@ -113,14 +149,14 @@ struct ascii_encoding
     static LEXY_CONSTEVAL int_type eof()
     {
         if constexpr (std::is_signed_v<char_type>)
-            return static_cast<int_type>(-1);
+            return int_type(-1);
         else
-            return static_cast<int_type>(0xFF);
+            return int_type(0xFF);
     }
 
     static LEXY_CONSTEVAL int_type to_int_type(char_type c)
     {
-        return static_cast<int_type>(c);
+        return int_type(c);
     }
 
     static constexpr std::size_t encode_code_point(code_point cp, char_type* buffer,
@@ -129,9 +165,35 @@ struct ascii_encoding
         LEXY_PRECONDITION(cp.is_ascii());
         LEXY_PRECONDITION(size >= 1);
 
-        *buffer = static_cast<char_type>(cp.value());
+        *buffer = char_type(cp.value());
         return 1;
     }
+
+    class code_point_decoder
+    {
+    public:
+        int init(int_type c)
+        {
+            if (c == eof())
+                return -1;
+
+            _result = char32_t(c);
+            return 0;
+        }
+
+        bool next(int_type)
+        {
+            return false;
+        }
+
+        auto finish() &&
+        {
+            return code_point(_result);
+        }
+
+    private:
+        char32_t _result = {};
+    };
 };
 
 /// An encoding where the input is assumed to be valid UTF-8.
@@ -146,12 +208,12 @@ struct utf8_encoding
     static LEXY_CONSTEVAL int_type eof()
     {
         // 0xFF is not part of valid UTF-8.
-        return static_cast<int_type>(0xFF);
+        return int_type(0xFF);
     }
 
     static LEXY_CONSTEVAL int_type to_int_type(char_type c)
     {
-        return static_cast<int_type>(c);
+        return int_type(c);
     }
 
     static constexpr std::size_t encode_code_point(code_point cp, char_type* buffer,
@@ -164,7 +226,7 @@ struct utf8_encoding
         {
             LEXY_PRECONDITION(size >= 1);
 
-            *buffer = static_cast<char_type>(cp.value());
+            buffer[0] = char_type(cp.value());
             return 1;
         }
         else if (cp.value() <= 0x07'FF)
@@ -174,8 +236,8 @@ struct utf8_encoding
             auto first  = (cp.value() >> 6) & 0x1F;
             auto second = (cp.value() >> 0) & 0x3F;
 
-            buffer[0] = 0xC0 | static_cast<char_type>(first);
-            buffer[1] = 0x80 | static_cast<char_type>(second);
+            buffer[0] = char_type(0xC0 | first);
+            buffer[1] = char_type(0x80 | second);
             return 2;
         }
         else if (cp.value() <= 0xFF'FF)
@@ -186,9 +248,9 @@ struct utf8_encoding
             auto second = (cp.value() >> 6) & 0x3F;
             auto third  = (cp.value() >> 0) & 0x3F;
 
-            buffer[0] = 0xE0 | static_cast<char_type>(first);
-            buffer[1] = 0x80 | static_cast<char_type>(second);
-            buffer[2] = 0x80 | static_cast<char_type>(third);
+            buffer[0] = char_type(0xE0 | first);
+            buffer[1] = char_type(0x80 | second);
+            buffer[2] = char_type(0x80 | third);
             return 3;
         }
         else
@@ -200,13 +262,82 @@ struct utf8_encoding
             auto third  = (cp.value() >> 6) & 0x3F;
             auto fourth = (cp.value() >> 0) & 0x3F;
 
-            buffer[0] = 0xF0 | static_cast<char_type>(first);
-            buffer[1] = 0x80 | static_cast<char_type>(second);
-            buffer[2] = 0x80 | static_cast<char_type>(third);
-            buffer[3] = 0x80 | static_cast<char_type>(fourth);
+            buffer[0] = char_type(0xF0 | first);
+            buffer[1] = char_type(0x80 | second);
+            buffer[2] = char_type(0x80 | third);
+            buffer[3] = char_type(0x80 | fourth);
             return 4;
         }
     }
+
+    class code_point_decoder
+    {
+        // Note that we don't care about overlong sequences,
+        // and invalid code points are detected at a later stage.
+        // As such, we just need to apply the masks on the leading bits.
+
+        static constexpr auto payload_lead1 = 0b0111'1111;
+        static constexpr auto payload_lead2 = 0b0001'1111;
+        static constexpr auto payload_lead3 = 0b0000'1111;
+        static constexpr auto payload_lead4 = 0b0000'0111;
+        static constexpr auto payload_cont  = 0b0011'1111;
+
+        static constexpr auto pattern_lead1 = 0b0 << 7;
+        static constexpr auto pattern_lead2 = 0b110 << 5;
+        static constexpr auto pattern_lead3 = 0b1110 << 4;
+        static constexpr auto pattern_lead4 = 0b11110 << 3;
+        static constexpr auto pattern_cont  = 0b10 << 6;
+
+    public:
+        int init(int_type c)
+        {
+            // If we don't have char8_t, int_type might be signed.
+            auto byte = static_cast<unsigned char>(c);
+            if ((byte & ~payload_lead1) == pattern_lead1)
+            {
+                _result = char32_t(c & payload_lead1);
+                return 0;
+            }
+            else if ((byte & ~payload_lead2) == pattern_lead2)
+            {
+                _result = char32_t(c & payload_lead2);
+                return 1;
+            }
+            else if ((byte & ~payload_lead3) == pattern_lead3)
+            {
+                _result = char32_t(c & payload_lead3);
+                return 2;
+            }
+            else if ((byte & ~payload_lead4) == pattern_lead4)
+            {
+                _result = char32_t(c & payload_lead4);
+                return 3;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        bool next(int_type c)
+        {
+            auto byte = static_cast<unsigned char>(c);
+            if ((byte & ~payload_cont) != pattern_cont)
+                return false; // Not a continuation byte.
+
+            _result <<= 6;
+            _result |= char32_t(c & payload_cont);
+            return true;
+        }
+
+        auto finish() &&
+        {
+            return code_point(_result);
+        }
+
+    private:
+        char32_t _result = {};
+    };
 };
 template <>
 constexpr bool utf8_encoding::is_secondary_char_type<char> = true;
@@ -228,7 +359,7 @@ struct utf16_encoding
 
     static LEXY_CONSTEVAL int_type to_int_type(char_type c)
     {
-        return static_cast<int_type>(c);
+        return int_type(c);
     }
 
     static constexpr std::size_t encode_code_point(code_point cp, char_type* buffer,
@@ -240,7 +371,7 @@ struct utf16_encoding
         {
             LEXY_PRECONDITION(size >= 1);
 
-            *buffer = static_cast<char_type>(cp.value());
+            buffer[0] = char_type(cp.value());
             return 1;
         }
         else
@@ -251,13 +382,69 @@ struct utf16_encoding
 
             auto u_prime       = cp.value() - 0x1'0000;
             auto high_ten_bits = u_prime >> 10;
-            auto low_ten_bits  = u_prime & 0b11'1111'1111;
+            auto low_ten_bits  = u_prime & 0b0000'0011'1111'1111;
 
-            buffer[0] = static_cast<char_type>(0xD800 + high_ten_bits);
-            buffer[1] = static_cast<char_type>(0xDC00 + low_ten_bits);
+            buffer[0] = char_type(0xD800 + high_ten_bits);
+            buffer[1] = char_type(0xDC00 + low_ten_bits);
             return 2;
         }
     }
+
+    class code_point_decoder
+    {
+        static constexpr auto payload_lead1 = 0b0000'0011'1111'1111;
+        static constexpr auto payload_lead2 = payload_lead1;
+
+        static constexpr auto pattern_lead1 = 0b110110 << 10;
+        static constexpr auto pattern_lead2 = 0b110111 << 10;
+
+    public:
+        int init(int_type c)
+        {
+            // We need to handle EOF separately and then convert it to a uint16_t.
+            if (c == eof())
+                return -1;
+            auto value = char_type(c);
+
+            if ((value & ~payload_lead1) == pattern_lead1)
+            {
+                _result = char32_t(value & payload_lead1);
+                return 1;
+            }
+            else if ((value & ~payload_lead2) == pattern_lead2)
+            {
+                return -1;
+            }
+            else
+            {
+                _result = char32_t(value);
+                return 0;
+            }
+        }
+
+        bool next(int_type c)
+        {
+            if (c == eof())
+                return false;
+            auto value = char_type(c);
+
+            if ((value & ~payload_lead2) != pattern_lead2)
+                return false;
+
+            _result <<= 10;
+            _result |= char32_t(value & payload_lead2);
+            _result |= 0x10000;
+            return true;
+        }
+
+        auto finish() &&
+        {
+            return code_point(_result);
+        }
+
+    private:
+        char32_t _result = {};
+    };
 };
 template <>
 constexpr bool utf16_encoding::is_secondary_char_type<wchar_t> = sizeof(wchar_t)
@@ -289,9 +476,33 @@ struct utf32_encoding
         LEXY_PRECONDITION(cp.is_valid());
         LEXY_PRECONDITION(size >= 1);
 
-        *buffer = static_cast<char_type>(cp.value());
+        *buffer = char_type(cp.value());
         return 1;
     }
+
+    class code_point_decoder
+    {
+    public:
+        int init(int_type c)
+        {
+            // No need to handle EOF, the code point validation will take care of that one.
+            _result = c;
+            return 0;
+        }
+
+        bool next(int_type)
+        {
+            return false;
+        }
+
+        auto finish() &&
+        {
+            return code_point(_result);
+        }
+
+    private:
+        char32_t _result = {};
+    };
 };
 template <>
 constexpr bool utf32_encoding::is_secondary_char_type<wchar_t> = sizeof(wchar_t)
@@ -313,12 +524,14 @@ struct raw_encoding
 
     static LEXY_CONSTEVAL int_type to_int_type(char_type c)
     {
-        return static_cast<int_type>(c);
+        return int_type(c);
     }
 
     static constexpr std::size_t encode_code_point(code_point cp, char_type* buffer,
                                                    std::size_t size)
         = delete;
+
+    class code_point_decoder;
 };
 template <>
 constexpr bool raw_encoding::is_secondary_char_type<char> = true;
--- test/encoding.cpp
+++ test/encoding.cpp
@@ -5,6 +5,7 @@
 #include <lexy/encoding.hpp>
 
 #include <doctest.h>
+#include <lexy/input/string_input.hpp>
 #include <string>
 
 TEST_CASE("wchar_t encoding")
@@ -79,6 +80,94 @@ TEST_CASE("encode code point")
         CHECK(encode(lexy::utf32_encoding{}, lexy::code_point(0x20AC)) == U"\u20AC");
         // 🙂
         CHECK(encode(lexy::utf32_encoding{}, lexy::code_point(0x1'F642)) == U"\U0001F642");
+    }
+}
+
+TEST_CASE("decode code point")
+{
+    auto decode = [](auto encoding, auto str, std::size_t length) {
+        auto input  = lexy::string_input<decltype(encoding)>(str, length);
+        auto reader = input.reader();
+
+        typename decltype(encoding)::code_point_decoder decoder;
+        auto                                            size = decoder.init(reader.peek());
+        if (size == -1)
+            return lexy::code_point();
+
+        for (auto i = 0; i < size; ++i)
+        {
+            reader.bump();
+            if (!decoder.next(reader.peek()))
+                return lexy::code_point();
+        }
+
+        return LEXY_MOV(decoder).finish();
+    };
+
+    SUBCASE("empty")
+    {
+        CHECK(!decode(lexy::default_encoding{}, nullptr, 0).is_valid());
+        CHECK(!decode(lexy::ascii_encoding{}, nullptr, 0).is_valid());
+        CHECK(!decode(lexy::utf8_encoding{}, nullptr, 0).is_valid());
+        CHECK(!decode(lexy::utf16_encoding{}, nullptr, 0).is_valid());
+        CHECK(!decode(lexy::utf32_encoding{}, nullptr, 0).is_valid());
+    }
+    SUBCASE("ASCII")
+    {
+        // ASCII is always guaranteed to round-trip.
+        for (auto c = 0u; c <= 0x7F; ++c)
+        {
+            char     str[]   = {char(c)};
+            char16_t str16[] = {char16_t(c)};
+            char32_t str32[] = {char32_t(c)};
+            auto     cp      = lexy::code_point(c);
+
+            CHECK(decode(lexy::default_encoding{}, str, 1) == cp);
+            CHECK(decode(lexy::ascii_encoding{}, str, 1) == cp);
+            CHECK(decode(lexy::utf8_encoding{}, str, 1) == cp);
+            CHECK(decode(lexy::utf16_encoding{}, str16, 1) == cp);
+            CHECK(decode(lexy::utf32_encoding{}, str32, 1) == cp);
+        }
+    }
+    SUBCASE("BMP")
+    {
+        // BMP is guaranteed to round-trip for UTF-16 and UTF-32.
+        for (auto c = 0u; c <= 0xFFFF; ++c)
+        {
+            auto cp = lexy::code_point(c);
+            if (!cp.is_valid())
+                continue; // Exclude surrogates.
+
+            char16_t str16[] = {char16_t(c)};
+            char32_t str32[] = {char32_t(c)};
+
+            CHECK(decode(lexy::utf16_encoding{}, str16, 1) == cp);
+            CHECK(decode(lexy::utf32_encoding{}, str32, 1) == cp);
+        }
+    }
+
+    SUBCASE("UTF-8 multi-unit sequences")
+    {
+        // ä - 2 bytes
+        CHECK(decode(lexy::utf8_encoding{}, u8"\u00E4", 2) == lexy::code_point(0x00E4));
+        // € - 3 bytes
+        CHECK(decode(lexy::utf8_encoding{}, u8"\u20AC", 3) == lexy::code_point(0x20AC));
+        // 🙂 - 4 bytes
+        CHECK(decode(lexy::utf8_encoding{}, u8"\U0001F642", 4) == lexy::code_point(0x1'F642));
+    }
+    SUBCASE("UTF-16 multi-unit sequences")
+    {
+        // €
+        CHECK(decode(lexy::utf16_encoding{}, u"\u20AC", 1) == lexy::code_point(0x20AC));
+        // 🙂
+        CHECK(decode(lexy::utf16_encoding{}, u"\U0001F642", 2) == lexy::code_point(0x1'F642));
+    }
+    SUBCASE("UTF-32 selected characters")
+    {
+        // €
+        CHECK(decode(lexy::utf32_encoding{}, U"\u20AC", 1) == lexy::code_point(0x20AC));
+        // 🙂
+        CHECK(decode(lexy::utf32_encoding{}, U"\U0001F642", 1) == lexy::code_point(0x1'F642));
     }
 }
 
