3a4466300947de5a96c5b5cb395388f2ec317aee
Jonathan Müller <git@foonathan.net>
2020-11-06 19:47:22 +0100

    Add fallback for make_index_sequence

--- include/lexy/_detail/integer_sequence.hpp
+++ include/lexy/_detail/integer_sequence.hpp
@@ -23,8 +23,37 @@ using make_index_sequence = __make_integer_seq<integer_sequence, std::size_t, Si
 #elif defined(__GNUC__) && __GNUC__ >= 8
 template <std::size_t Size>
 using make_index_sequence = index_sequence<__integer_pack(Size)...>;
-#else
+#elif defined(_MSC_VER)
 #    error TODO
+#else
+
+// Adapted from https://stackoverflow.com/a/32223343.
+template <class Sequence1, class Sequence2>
+struct concat_seq;
+template <std::size_t... I1, std::size_t... I2>
+struct concat_seq<index_sequence<I1...>, index_sequence<I2...>>
+{
+    using type = index_sequence<I1..., (sizeof...(I1) + I2)...>;
+};
+
+template <size_t N>
+struct _make_index_sequence : concat_seq<typename _make_index_sequence<N / 2>::type,
+                                         typename _make_index_sequence<N - N / 2>::type>
+{};
+template <>
+struct _make_index_sequence<0>
+{
+    using type = index_sequence<>;
+};
+template <>
+struct _make_index_sequence<1>
+{
+    using type = index_sequence<0>;
+};
+
+template <std::size_t Size>
+using make_index_sequence = typename _make_index_sequence<Size>::type;
+
 #endif
 
 template <typename... T>
