2f51ed260b7bdfd5aaee24fcbb61deb5090ec142
Jonathan Müller <git@foonathan.net>
2020-11-25 20:31:13 +0100

    Re-implement BOM to support arbitrary encodings

--- include/lexy/dsl/bom.hpp
+++ include/lexy/dsl/bom.hpp
@@ -6,57 +6,85 @@
 #define LEXY_DSL_BOM_HPP_INCLUDED
 
 #include <lexy/dsl/base.hpp>
-#include <lexy/dsl/literal.hpp>
-#include <lexy/dsl/sequence.hpp>
 
 namespace lexyd
 {
 template <typename Encoding, lexy::encoding_endianness Endianness>
 struct _bom_impl
 {
+    static_assert(Endianness != lexy::encoding_endianness::bom,
+                  "bom with BOM-endianness doesn't make sense");
+
+    static constexpr auto name = "";
+
     static constexpr const unsigned char* value  = nullptr;
     static constexpr std::size_t          length = 0u;
 };
 template <lexy::encoding_endianness DontCare>
 struct _bom_impl<lexy::utf8_encoding, DontCare>
 {
+    static constexpr auto name = "BOM.UTF-8";
+
     static constexpr unsigned char value[] = {0xEF, 0xBB, 0xBF};
     static constexpr auto          length  = 3u;
 };
 template <>
 struct _bom_impl<lexy::utf16_encoding, lexy::encoding_endianness::little>
 {
+    static constexpr auto name = "BOM.UTF-16-LE";
+
     static constexpr unsigned char value[] = {0xFF, 0xFE};
     static constexpr auto          length  = 2u;
 };
 template <>
 struct _bom_impl<lexy::utf16_encoding, lexy::encoding_endianness::big>
 {
+    static constexpr auto name = "BOM.UTF-16-BE";
+
     static constexpr unsigned char value[] = {0xFE, 0xFF};
     static constexpr auto          length  = 2u;
 };
 template <>
 struct _bom_impl<lexy::utf32_encoding, lexy::encoding_endianness::little>
 {
+    static constexpr auto name = "BOM.UTF-32-LE";
+
     static constexpr unsigned char value[] = {0xFF, 0xFE, 0x00, 0x00};
     static constexpr auto          length  = 4u;
 };
 template <>
 struct _bom_impl<lexy::utf32_encoding, lexy::encoding_endianness::big>
 {
+    static constexpr auto name = "BOM.UTF-32-BE";
+
     static constexpr unsigned char value[] = {0x00, 0x00, 0xFE, 0xFF};
     static constexpr auto          length  = 4u;
 };
 
 template <typename Encoding, lexy::encoding_endianness Endianness>
-struct _bom : _lit<_bom<Encoding, Endianness>>
+struct _bom : atom_base<_bom<Encoding, Endianness>>
 {
-    using char_type = unsigned char;
+    using _impl = _bom_impl<Encoding, Endianness>;
 
-    static LEXY_CONSTEVAL auto get()
+    template <typename Reader>
+    LEXY_DSL_FUNC bool match(Reader& reader)
     {
-        using impl = _bom_impl<Encoding, Endianness>;
-        return lexy::_detail::basic_string_view<unsigned char>(impl::value, impl::length);
+        constexpr auto string
+            = lexy::_detail::basic_string_view<unsigned char>(_impl::value, _impl::length);
+        for (auto c : string)
+        {
+            if (reader.peek() != Reader::encoding::to_int_type(typename Reader::char_type(c)))
+                return false;
+            reader.bump();
+        }
+
+        return true;
+    }
+
+    template <typename Reader>
+    LEXY_DSL_FUNC auto error(const Reader&, typename Reader::iterator pos)
+    {
+        return lexy::make_error<Reader, lexy::expected_char_class>(pos, _impl::name);
     }
 };
 
--- tests/lexy/dsl/bom.cpp
+++ tests/lexy/dsl/bom.cpp
@@ -4,3 +4,97 @@
 
 #include <lexy/dsl/bom.hpp>
 
+#include "verify.hpp"
+
+TEST_CASE("dsl::bom")
+{
+    SUBCASE("no bom")
+    {
+        constexpr auto atom
+            = lexy::dsl::bom<lexy::default_encoding, lexy::encoding_endianness::little>;
+        CHECK(lexy::is_pattern<decltype(atom)>);
+
+        constexpr auto empty = atom_matches(atom, "");
+        CHECK(empty);
+        CHECK(empty.count == 0);
+    }
+    SUBCASE("UTF-8")
+    {
+        constexpr auto atom
+            = lexy::dsl::bom<lexy::utf8_encoding, lexy::encoding_endianness::little>;
+        CHECK(lexy::is_pattern<decltype(atom)>);
+
+        constexpr auto empty = atom_matches(atom, "");
+        CHECK(!empty);
+        CHECK(empty.count == 0);
+        CHECK(empty.error.character_class() == "BOM.UTF-8");
+
+        static constexpr char str[] = {char(0xEF), char(0xBB), char(0xBF)};
+        constexpr auto        bom   = atom_matches(atom, str, 3);
+        CHECK(bom);
+        CHECK(bom.count == 3);
+    }
+    SUBCASE("UTF-16 little")
+    {
+        constexpr auto atom
+            = lexy::dsl::bom<lexy::utf16_encoding, lexy::encoding_endianness::little>;
+        CHECK(lexy::is_pattern<decltype(atom)>);
+
+        constexpr auto empty = atom_matches(atom, "");
+        CHECK(!empty);
+        CHECK(empty.count == 0);
+        CHECK(empty.error.character_class() == "BOM.UTF-16-LE");
+
+        static constexpr char str[] = {char(0xFF), char(0xFE)};
+        constexpr auto        bom   = atom_matches(atom, str, 2);
+        CHECK(bom);
+        CHECK(bom.count == 2);
+    }
+    SUBCASE("UTF-16 big")
+    {
+        constexpr auto atom = lexy::dsl::bom<lexy::utf16_encoding, lexy::encoding_endianness::big>;
+        CHECK(lexy::is_pattern<decltype(atom)>);
+
+        constexpr auto empty = atom_matches(atom, "");
+        CHECK(!empty);
+        CHECK(empty.count == 0);
+        CHECK(empty.error.character_class() == "BOM.UTF-16-BE");
+
+        static constexpr char str[] = {char(0xFE), char(0xFF)};
+        constexpr auto        bom   = atom_matches(atom, str, 2);
+        CHECK(bom);
+        CHECK(bom.count == 2);
+    }
+    SUBCASE("UTF-32 little")
+    {
+        constexpr auto atom
+            = lexy::dsl::bom<lexy::utf32_encoding, lexy::encoding_endianness::little>;
+        CHECK(lexy::is_pattern<decltype(atom)>);
+
+        constexpr auto empty = atom_matches(atom, "");
+        CHECK(!empty);
+        CHECK(empty.count == 0);
+        CHECK(empty.error.character_class() == "BOM.UTF-32-LE");
+
+        static constexpr char str[] = {char(0xFF), char(0xFE), 0, 0};
+        constexpr auto        bom   = atom_matches(atom, str, 4);
+        CHECK(bom);
+        CHECK(bom.count == 4);
+    }
+    SUBCASE("UTF-32 big")
+    {
+        constexpr auto atom = lexy::dsl::bom<lexy::utf32_encoding, lexy::encoding_endianness::big>;
+        CHECK(lexy::is_pattern<decltype(atom)>);
+
+        constexpr auto empty = atom_matches(atom, "");
+        CHECK(!empty);
+        CHECK(empty.count == 0);
+        CHECK(empty.error.character_class() == "BOM.UTF-32-BE");
+
+        static constexpr char str[] = {0, 0, char(0xFE), char(0xFF)};
+        constexpr auto        bom   = atom_matches(atom, str, 4);
+        CHECK(bom);
+        CHECK(bom.count == 4);
+    }
+}
+
