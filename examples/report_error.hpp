cfea6413c7ef384d571e74ad9365a5c2eafcfd8a
Jonathan Müller <git@foonathan.net>
2020-11-30 21:49:28 +0100

    Fix bug with `lexy_ex::report_error()`

--- examples/report_error.hpp
+++ examples/report_error.hpp
@@ -23,7 +23,11 @@ void _print_location(const lexy::error_location<Reader>& location)
     for (auto c : location.context)
         std::fputc(c, stderr);
     std::fputc('\n', stderr);
+}
 
+template <typename Reader>
+void _print_message_indent(const lexy::error_location<Reader>& location)
+{
     std::fputs("     | ", stderr);
     for (auto i = 0u; i != location.column - 1; ++i)
         std::fputc(' ', stderr);
@@ -31,9 +35,9 @@ void _print_location(const lexy::error_location<Reader>& location)
     // The next character printed will be under the error location of the column above.
 }
 
+// Print a generic error.
 template <typename Reader, typename Tag>
-void _print_location_msg(const lexy::error_location<Reader>& location,
-                         const lexy::error<Reader, Tag>&     e)
+void _print_message(const lexy::error_location<Reader>& location, const lexy::error<Reader, Tag>& e)
 {
     if (e.begin() == e.end())
         std::fputc('^', stderr);
@@ -49,28 +53,35 @@ void _print_location_msg(const lexy::error_location<Reader>& location,
 
     std::fprintf(stderr, " %.*s", int(e.message().size()), e.message().data());
 }
+
+// Print an expected_literal error.
 template <typename Reader>
-void _print_location_msg(const lexy::error_location<Reader>&,
-                         const lexy::error<Reader, lexy::expected_literal>& e)
+void _print_message(const lexy::error_location<Reader>&,
+                    const lexy::error<Reader, lexy::expected_literal>& e)
 {
     std::fprintf(stderr, "^ expected '%.*s'", int(e.string().size()),
                  reinterpret_cast<const char*>(e.string().data()));
 }
+
+// Print an expected_char_class error.
 template <typename Reader>
-void _print_location_msg(const lexy::error_location<Reader>&,
-                         const lexy::error<Reader, lexy::expected_char_class>& e)
+void _print_message(const lexy::error_location<Reader>&,
+                    const lexy::error<Reader, lexy::expected_char_class>& e)
 {
     std::fprintf(stderr, "^ expected '%.*s' character", int(e.character_class().size()),
                  e.character_class().data());
 }
 
+// The error callback that prints to stderr.
 constexpr auto report_error = lexy::callback([](const auto& context, const auto& error) {
+    // Convert the context location and error location into line/column information.
     auto context_location
         = lexy::make_error_location(context.input(), context.position(),
                                     lexy::dsl::ascii::character, lexy::dsl::newline);
     auto location = lexy::make_error_location(context.input(), error.position(),
                                               lexy::dsl::ascii::character, lexy::dsl::newline);
 
+    // Print the main error headline.
     auto prod_name = context.production();
     std::fflush(stdout);
     std::fprintf(stderr, "error: while parsing %.*s\n", int(prod_name.size()), prod_name.data());
@@ -78,17 +89,20 @@ constexpr auto report_error = lexy::callback([](const auto& context, const auto&
     if (location.line != context_location.line)
     {
         _print_location(context_location);
+        _print_message_indent(context_location);
         std::fputs("^ beginning here\n", stderr);
         _print_location(location);
+        _print_message_indent(location);
     }
     else
     {
-        _print_location(context_location);
+        _print_location(location);
+        _print_message_indent(context_location);
         for (auto i = context_location.column; i != location.column; ++i)
             std::fputc('~', stderr);
     }
 
-    _print_location_msg(location, error);
+    _print_message(location, error);
     std::putc('\n', stderr);
 });
 } // namespace lexy_ex
