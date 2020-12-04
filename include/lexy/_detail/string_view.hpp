a5a1dbf6f99f768f6154276a04867d62c5ebd8ec
Jonathan Müller <git@foonathan.net>
2020-12-04 21:57:39 +0100

    Workaround MSVC invalidating end iterator of compile-time string
    See https://developercommunity.visualstudio.com/content/problem/1276953/gf-invalidates-end-iterators-for-constexpr-string.html
    
    For `string_view`, we can switch to ptr + size.
    This isn't possible for `lexeme`, so we disable compile-time execution.

--- include/lexy/_detail/string_view.hpp
+++ include/lexy/_detail/string_view.hpp
@@ -17,66 +17,68 @@ public:
     using char_type = CharT;
 
     //=== constructor ===//
-    constexpr basic_string_view() noexcept : _begin(nullptr), _end(nullptr) {}
+    constexpr basic_string_view() noexcept : _ptr(nullptr), _size(0u) {}
 
-    constexpr basic_string_view(const char_type* str) noexcept : _begin(str), _end(str)
+    constexpr basic_string_view(const char_type* str) noexcept : _ptr(str), _size(0u)
     {
-        while (*_end)
-            ++_end;
+        while (*str++)
+            ++_size;
     }
 
     constexpr basic_string_view(const char_type* ptr, std::size_t size) noexcept
-    : _begin(ptr), _end(ptr + size)
+    : _ptr(ptr), _size(size)
     {}
 
     constexpr basic_string_view(const char_type* begin, const char_type* end) noexcept
-    : _begin(begin), _end(end)
-    {}
+    : _ptr(begin), _size(std::size_t(end - begin))
+    {
+        LEXY_PRECONDITION(begin <= end);
+    }
 
     //=== access ===//
     using iterator = const char_type*;
 
     constexpr iterator begin() const noexcept
     {
-        return _begin;
+        return _ptr;
     }
     constexpr iterator end() const noexcept
     {
-        return _end;
+        return _ptr + _size;
     }
 
     constexpr bool empty() const noexcept
     {
-        return _begin == _end;
+        return _size == 0u;
     }
     constexpr std::size_t size() const noexcept
     {
-        return static_cast<std::size_t>(_end - _begin);
+        return _size;
     }
     constexpr std::size_t length() const noexcept
     {
-        return static_cast<std::size_t>(_end - _begin);
+        return _size;
     }
 
     constexpr char_type operator[](std::size_t i) const noexcept
     {
-        LEXY_PRECONDITION(i <= size());
-        return _begin[i];
+        LEXY_PRECONDITION(i <= _size);
+        return _ptr[i];
     }
     constexpr char_type front() const noexcept
     {
         LEXY_PRECONDITION(!empty());
-        return *_begin;
+        return *_ptr;
     }
     constexpr char_type back() const noexcept
     {
         LEXY_PRECONDITION(!empty());
-        return *(_end - 1);
+        return _ptr[_size - 1];
     }
 
     constexpr const char_type* data() const noexcept
     {
-        return _begin;
+        return _ptr;
     }
 
     //=== operations ===//
@@ -84,22 +86,27 @@ public:
 
     constexpr void remove_prefix(std::size_t n) noexcept
     {
-        LEXY_PRECONDITION(n <= size());
-        _begin += n;
+        LEXY_PRECONDITION(n <= _size);
+        _ptr += n;
+        _size -= n;
     }
     constexpr void remove_suffix(std::size_t n) noexcept
     {
-        LEXY_PRECONDITION(n <= size());
-        _end -= n;
+        LEXY_PRECONDITION(n <= _size);
+        _size -= n;
     }
 
     constexpr basic_string_view substr(std::size_t pos, std::size_t length = npos) const noexcept
     {
-        LEXY_PRECONDITION(pos < size());
-        if (length >= size() - pos)
-            return basic_string_view(_begin + pos, _end);
+        LEXY_PRECONDITION(pos < _size);
+        if (length >= _size - pos)
+        {
+            auto begin = _ptr + pos;
+            auto end   = _ptr + _size;
+            return basic_string_view(begin, end);
+        }
         else
-            return basic_string_view(_begin + pos, _begin + pos + length);
+            return basic_string_view(_ptr + pos, length);
     }
     constexpr std::size_t find(basic_string_view str, std::size_t pos = 0) const noexcept
     {
@@ -137,8 +144,8 @@ public:
     }
 
 protected:
-    const CharT* _begin;
-    const CharT* _end;
+    const CharT* _ptr;
+    std::size_t  _size;
 };
 using string_view = basic_string_view<char>;
 } // namespace lexy::_detail
--- tests/lexy/error_location.cpp
+++ tests/lexy/error_location.cpp
@@ -25,6 +25,13 @@ struct str_context
 };
 } // namespace
 
+// https://developercommunity2.visualstudio.com/t/GF--invalidates-end-iterators-for-const/1276953
+#if !defined(__clang__) && defined(_MSC_VER)
+#    define TEST_CONSTEXPR
+#else
+#    define TEST_CONSTEXPR constexpr
+#endif
+
 TEST_CASE("make_error_location")
 {
     constexpr auto character = lexy::dsl::ascii::character;
@@ -32,52 +39,54 @@ TEST_CASE("make_error_location")
 
     SUBCASE("basic")
     {
-        constexpr auto input = lexy::zstring_input("Line 1\n"
-                                                   "Line 2\n"
-                                                   "Line 3\n");
+        TEST_CONSTEXPR auto input = lexy::zstring_input("Line 1\n"
+                                                        "Line 2\n"
+                                                        "Line 3\n");
 
-        constexpr auto first_1
+        TEST_CONSTEXPR auto first_1
             = lexy::make_error_location(input, input.begin(), character, newline);
         CHECK(first_1.line == 1);
         CHECK(first_1.column == 1);
         CHECK(first_1.context == str_context{"Line 1"});
 
-        constexpr auto first_2
+        TEST_CONSTEXPR auto first_2
             = lexy::make_error_location(input, input.begin() + 4, character, newline);
         CHECK(first_2.line == 1);
         CHECK(first_2.column == 5);
         CHECK(first_2.context == str_context{"Line 1"});
 
-        constexpr auto second
+        TEST_CONSTEXPR auto second
             = lexy::make_error_location(input, input.begin() + 10, character, newline);
         CHECK(second.line == 2);
         CHECK(second.column == 4);
         CHECK(second.context == str_context{"Line 2"});
 
-        constexpr auto third
+        TEST_CONSTEXPR auto third
             = lexy::make_error_location(input, input.begin() + 17, character, newline);
         CHECK(third.line == 3);
         CHECK(third.column == 4);
         CHECK(third.context == str_context{"Line 3"});
 
-        constexpr auto nl = lexy::make_error_location(input, input.begin() + 6, character, newline);
+        TEST_CONSTEXPR auto nl
+            = lexy::make_error_location(input, input.begin() + 6, character, newline);
         CHECK(input.begin()[6] == '\n');
         CHECK(nl.line == 1);
         CHECK(nl.column == 7);
         CHECK(nl.context == str_context{"Line 1"});
 
-        constexpr auto last = lexy::make_error_location(input, input.end() - 1, character, newline);
+        TEST_CONSTEXPR auto last
+            = lexy::make_error_location(input, input.end() - 1, character, newline);
         CHECK(last.line == 3);
         CHECK(last.column == 7);
         CHECK(last.context == str_context{"Line 3"});
 
-        constexpr auto end = lexy::make_error_location(input, input.end(), character, newline);
+        TEST_CONSTEXPR auto end = lexy::make_error_location(input, input.end(), character, newline);
         CHECK(end.line == 4);
         CHECK(end.column == 1);
         CHECK(end.context.empty());
 
         // +1 is ok, input is null-terminated so it is the real one past location of the array
-        constexpr auto out_of_bounds
+        TEST_CONSTEXPR auto out_of_bounds
             = lexy::make_error_location(input, input.end() + 1, character, newline);
         CHECK(out_of_bounds.line == 4);
         CHECK(out_of_bounds.column == 1);
@@ -85,46 +94,48 @@ TEST_CASE("make_error_location")
     }
     SUBCASE("no trailing newline")
     {
-        constexpr auto input = lexy::zstring_input("Line 1\n"
-                                                   "Line 2\n"
-                                                   "Line 3");
+        TEST_CONSTEXPR auto input = lexy::zstring_input("Line 1\n"
+                                                        "Line 2\n"
+                                                        "Line 3");
 
-        constexpr auto first_1
+        TEST_CONSTEXPR auto first_1
             = lexy::make_error_location(input, input.begin(), character, newline);
         CHECK(first_1.line == 1);
         CHECK(first_1.column == 1);
         CHECK(first_1.context == str_context{"Line 1"});
 
-        constexpr auto first_2
+        TEST_CONSTEXPR auto first_2
             = lexy::make_error_location(input, input.begin() + 4, character, newline);
         CHECK(first_2.line == 1);
         CHECK(first_2.column == 5);
         CHECK(first_2.context == str_context{"Line 1"});
 
-        constexpr auto second
+        TEST_CONSTEXPR auto second
             = lexy::make_error_location(input, input.begin() + 10, character, newline);
         CHECK(second.line == 2);
         CHECK(second.column == 4);
         CHECK(second.context == str_context{"Line 2"});
 
-        constexpr auto third
+        TEST_CONSTEXPR auto third
             = lexy::make_error_location(input, input.begin() + 17, character, newline);
         CHECK(third.line == 3);
         CHECK(third.column == 4);
         CHECK(third.context == str_context{"Line 3"});
 
-        constexpr auto nl = lexy::make_error_location(input, input.begin() + 6, character, newline);
+        TEST_CONSTEXPR auto nl
+            = lexy::make_error_location(input, input.begin() + 6, character, newline);
         CHECK(input.begin()[6] == '\n');
         CHECK(nl.line == 1);
         CHECK(nl.column == 7);
         CHECK(nl.context == str_context{"Line 1"});
 
-        constexpr auto last = lexy::make_error_location(input, input.end() - 1, character, newline);
+        TEST_CONSTEXPR auto last
+            = lexy::make_error_location(input, input.end() - 1, character, newline);
         CHECK(last.line == 3);
         CHECK(last.column == 6);
         CHECK(last.context == str_context{"Line 3"});
 
-        constexpr auto end = lexy::make_error_location(input, input.end(), character, newline);
+        TEST_CONSTEXPR auto end = lexy::make_error_location(input, input.end(), character, newline);
         CHECK(end.line == 3);
         CHECK(end.column == 7);
         CHECK(end.context == str_context{"Line 3"});
@@ -133,21 +144,21 @@ TEST_CASE("make_error_location")
     {
         // 0xFF isn't an ascii character
         static constexpr char array[] = {'a', 'b', 'c', char(0xFF), 'd', '\0'};
-        constexpr auto        input   = lexy::zstring_input(array);
+        TEST_CONSTEXPR auto   input   = lexy::zstring_input(array);
 
-        constexpr auto before
+        TEST_CONSTEXPR auto before
             = lexy::make_error_location(input, input.begin() + 2, character, newline);
         CHECK(before.line == 1);
         CHECK(before.column == 3);
         CHECK(before.context.size() == 5);
 
-        constexpr auto weird
+        TEST_CONSTEXPR auto weird
             = lexy::make_error_location(input, input.begin() + 3, character, newline);
         CHECK(weird.line == 1);
         CHECK(weird.column == 4);
         CHECK(weird.context.size() == 5);
 
-        constexpr auto after
+        TEST_CONSTEXPR auto after
             = lexy::make_error_location(input, input.begin() + 4, character, newline);
         CHECK(after.line == 1);
         CHECK(after.column == 4);
