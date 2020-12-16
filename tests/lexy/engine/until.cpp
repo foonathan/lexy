ad944e3a7b9c9f578fe26eda07d2c07d3507f904
Jonathan Müller <git@foonathan.net>
2020-12-16 18:19:23 +0100

    Add `lexy::engine_until[_eof]`

---
+++ tests/lexy/engine/until.cpp
@@ -0,0 +1,86 @@
+// Copyright (C) 2020 Jonathan Müller <jonathanmueller.dev@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#include <lexy/engine/until.hpp>
+
+#include "verify.hpp"
+#include <lexy/_detail/nttp_string.hpp>
+#include <lexy/engine/literal.hpp>
+
+namespace
+{
+constexpr auto trie_ab = lexy::linear_trie<LEXY_NTTP_STRING("ab")>;
+}
+
+TEST_CASE("engine_until")
+{
+    using condition = lexy::engine_literal<trie_ab>;
+    using engine    = lexy::engine_until<condition>;
+    CHECK(lexy::engine_is_matcher<engine>);
+
+    constexpr auto empty = engine_matches<engine>("");
+    CHECK(!empty);
+    CHECK(empty.count == 0);
+    CHECK(empty.ec == condition::index_to_error(0));
+
+    constexpr auto zero = engine_matches<engine>("ab");
+    CHECK(zero);
+    CHECK(zero.count == 2);
+    constexpr auto one = engine_matches<engine>("-ab");
+    CHECK(one);
+    CHECK(one.count == 3);
+    constexpr auto two = engine_matches<engine>("-+ab");
+    CHECK(two);
+    CHECK(two.count == 4);
+    constexpr auto three = engine_matches<engine>("+-+ab");
+    CHECK(three);
+    CHECK(three.count == 5);
+
+    constexpr auto partial = engine_matches<engine>("+a+ab");
+    CHECK(partial);
+    CHECK(partial.count == 5);
+
+    constexpr auto partial_end = engine_matches<engine>("+a+a");
+    CHECK(!partial_end);
+    CHECK(partial_end.count == 4);
+    CHECK(partial_end.ec == condition::index_to_error(0));
+
+    constexpr auto unterminated = engine_matches<engine>("++");
+    CHECK(!unterminated);
+    CHECK(unterminated.count == 2);
+    CHECK(unterminated.ec == condition::index_to_error(0));
+}
+
+TEST_CASE("engine_until_eof")
+{
+    using condition = lexy::engine_literal<trie_ab>;
+    using engine    = lexy::engine_until_eof<condition>;
+    CHECK(lexy::engine_is_matcher<engine>);
+
+    constexpr auto empty = engine_matches<engine>("");
+    CHECK(empty);
+    CHECK(empty.count == 0);
+
+    constexpr auto zero = engine_matches<engine>("ab");
+    CHECK(zero);
+    CHECK(zero.count == 2);
+    constexpr auto one = engine_matches<engine>("-ab");
+    CHECK(one);
+    CHECK(one.count == 3);
+    constexpr auto two = engine_matches<engine>("-+ab");
+    CHECK(two);
+    CHECK(two.count == 4);
+    constexpr auto three = engine_matches<engine>("+-+ab");
+    CHECK(three);
+    CHECK(three.count == 5);
+
+    constexpr auto partial = engine_matches<engine>("+a+ab");
+    CHECK(partial);
+    CHECK(partial.count == 5);
+
+    constexpr auto partial_end = engine_matches<engine>("+a+a");
+    CHECK(partial_end);
+    CHECK(partial_end.count == 4);
+}
+
