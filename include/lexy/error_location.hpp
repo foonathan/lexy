07d6d486c09dbb1636eb933800a67c02bf87adca
Jonathan Müller <git@foonathan.net>
2020-12-22 19:24:32 +0100

    Require tokens instead of patterns for `lexy::make_error_location()`

--- include/lexy/error_location.hpp
+++ include/lexy/error_location.hpp
@@ -22,11 +22,13 @@ struct error_location
 template <typename Input>
 using error_location_for = error_location<input_reader<Input>>;
 
-template <typename Input, typename PatternCP, typename PatternNL>
+template <typename Input, typename TokenCP, typename TokenNL>
 constexpr auto make_error_location(const Input& input, typename input_reader<Input>::iterator pos,
-                                   PatternCP, PatternNL) -> error_location_for<Input>
+                                   TokenCP, TokenNL) -> error_location_for<Input>
 {
-    static_assert(is_pattern<PatternCP> && is_pattern<PatternNL>);
+    static_assert(is_token<TokenCP> && is_token<TokenNL>);
+    using engine_cp = typename TokenCP::token_engine;
+    using engine_nl = typename TokenNL::token_engine;
 
     auto reader = input.reader();
 
@@ -42,14 +44,14 @@ constexpr auto make_error_location(const Input& input, typename input_reader<Inp
             // We found the position of the error.
             break;
         }
-        else if (PatternNL::matcher::match(reader))
+        else if (lexy::engine_try_match<engine_nl>(reader))
         {
             // We're at a new line.
             ++cur_line;
             cur_column = 1;
             line_start = reader.cur();
         }
-        else if (PatternCP::matcher::match(reader))
+        else if (lexy::engine_try_match<engine_cp>(reader))
         {
             // Next column.
             ++cur_column;
@@ -72,23 +74,12 @@ constexpr auto make_error_location(const Input& input, typename input_reader<Inp
     // We now need to find the end of the current line to compute context.
     while (true)
     {
-        auto save = reader;
-        if (PatternNL::matcher::match(reader))
-        {
-            // We've found the final newline, reset to previous position.
-            reader = LEXY_MOV(save);
+        if (lexy::engine_peek<engine_nl>(reader))
             break;
-        }
         else if (reader.eof())
-        {
-            // Last line of the input.
             break;
-        }
         else
-        {
-            // Try again.
             reader.bump();
-        }
     }
 
     // We've found the right line and column number and the reader is at the end of the line.
