400a1b93eb153383b3a9c4a1b33a71f53bce6a3b
Jonathan Müller <git@foonathan.net>
2021-06-13 10:12:27 +0200

    Remove deprecated test

--- tests/lexy/dsl/whitespace.cpp
+++ tests/lexy/dsl/whitespace.cpp
@@ -2,7 +2,6 @@
 // This file is subject to the license terms in the LICENSE file
 // found in the top-level directory of this distribution.
 
-#define LEXY_IGNORE_DEPRECATED_WHITESPACE
 #include <lexy/dsl/whitespace.hpp>
 
 #include "verify.hpp"
@@ -335,78 +334,6 @@ TEST_CASE("automatic whitespace")
 
         auto inner_space = LEXY_VERIFY_PRODUCTION(ws_production, "a bc");
         CHECK(inner_space == -1);
-    }
-}
-
-TEST_CASE("dsl::whitespaced()")
-{
-    SUBCASE("simple")
-    {
-        static constexpr auto rule = whitespaced(LEXY_LIT("abc"), LEXY_LIT(" "));
-        CHECK(lexy::is_rule<decltype(rule)>);
-        CHECK(lexy::is_branch<decltype(rule)>);
-
-        struct callback
-        {
-            const char* str;
-
-            LEXY_VERIFY_FN int success(const char* cur)
-            {
-                return int(cur - str) - 3;
-            }
-
-            LEXY_VERIFY_FN int error(test_error<lexy::expected_literal> e)
-            {
-                LEXY_VERIFY_CHECK(e.string() == lexy::_detail::string_view("abc"));
-                return -1;
-            }
-        };
-
-        auto empty = LEXY_VERIFY("");
-        CHECK(empty == -1);
-
-        auto abc = LEXY_VERIFY("abc");
-        CHECK(abc == 0);
-
-        auto space_abc = LEXY_VERIFY(" abc");
-        CHECK(space_abc == 1);
-        auto space_space_abc = LEXY_VERIFY("  abc");
-        CHECK(space_space_abc == 2);
-    }
-    SUBCASE("branch")
-    {
-        static constexpr auto rule_
-            = whitespaced(LEXY_LIT("abc") >> lexy::dsl::id<0>, LEXY_LIT(" "));
-        CHECK(lexy::is_rule<decltype(rule_)>);
-        CHECK(lexy::is_branch<decltype(rule_)>);
-
-        static constexpr auto rule = if_(rule_);
-
-        struct callback
-        {
-            const char* str;
-
-            LEXY_VERIFY_FN int success(const char* cur)
-            {
-                LEXY_VERIFY_CHECK(cur == str);
-                return 0;
-            }
-            LEXY_VERIFY_FN int success(const char* cur, lexy::id<0>)
-            {
-                return int(cur - str);
-            }
-        };
-
-        auto empty = LEXY_VERIFY("");
-        CHECK(empty == 0);
-
-        auto abc = LEXY_VERIFY("abc");
-        CHECK(abc == 3);
-
-        auto space_abc = LEXY_VERIFY(" abc");
-        CHECK(space_abc == 4);
-        auto space_space_abc = LEXY_VERIFY("  abc");
-        CHECK(space_space_abc == 5);
     }
 }
 
