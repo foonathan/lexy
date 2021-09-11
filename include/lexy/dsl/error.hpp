7f7194b75b8f2be169346a637de6c230453ce673
Jonathan Müller <git@foonathan.net>
2021-09-11 22:11:05 +0200

    Remove deprecated `dsl::require()/prevent()`

--- include/lexy/dsl/error.hpp
+++ include/lexy/dsl/error.hpp
@@ -9,13 +9,6 @@
 #include <lexy/dsl/branch.hpp>
 #include <lexy/dsl/token.hpp>
 
-#ifdef LEXY_IGNORE_DEPRECATED_REQUIRE
-#    define LEXY_DEPRECATED_REQUIRE
-#else
-#    define LEXY_DEPRECATED_REQUIRE                                                                \
-        [[deprecated("`dsl::require()/dsl::prevent()` have been replaced by `dsl::peek[_not]()`")]]
-#endif
-
 namespace lexyd
 {
 template <typename Tag, typename Token>
@@ -112,103 +105,6 @@ constexpr auto must(Branch)
     static_assert(lexy::is_branch_rule<Branch>);
     static_assert(!Branch::is_unconditional_branch);
     return _must_dsl<Branch>{};
-}
-} // namespace lexyd
-
-namespace lexyd
-{
-template <typename Token, typename Tag>
-struct _require : rule_base
-{
-    template <typename NextParser>
-    struct parser
-    {
-        template <typename Context, typename Reader, typename... Args>
-        LEXY_DSL_FUNC bool parse(Context& context, Reader& reader, Args&&... args)
-        {
-            if (lexy::engine_peek<typename Token::token_engine>(reader))
-            {
-                // Token did match as we want, so continue normally.
-                return NextParser::parse(context, reader, LEXY_FWD(args)...);
-            }
-            else
-            {
-                // Token did not match, report the correct error, but continue normally.
-                auto err = lexy::make_error<Reader, Tag>(reader.cur());
-                context.on(_ev::error{}, err);
-                return NextParser::parse(context, reader, LEXY_FWD(args)...);
-            }
-        }
-    };
-};
-
-template <typename Token, typename Tag>
-struct _prevent : rule_base
-{
-    template <typename NextParser>
-    struct parser
-    {
-        template <typename Context, typename Reader, typename... Args>
-        LEXY_DSL_FUNC bool parse(Context& context, Reader& reader, Args&&... args)
-        {
-            auto begin = reader.cur();
-            auto copy  = reader;
-            if (lexy::engine_try_match<typename Token::token_engine>(copy))
-            {
-                // Token did match what we don't want.
-                // Report an error, but continue parsing.
-                auto err = lexy::make_error<Reader, Tag>(begin, copy.cur());
-                context.on(_ev::error{}, err);
-                return NextParser::parse(context, reader, LEXY_FWD(args)...);
-            }
-            else
-            {
-                // Token did not match, so we didn't consume it.
-                // Continue normally.
-                return NextParser::parse(context, reader, LEXY_FWD(args)...);
-            }
-        }
-    };
-};
-
-template <typename Token>
-struct _require_dsl
-{
-    template <typename Tag>
-    static constexpr _require<Token, Tag> error = _require<Token, Tag>{};
-};
-template <typename Token>
-struct _prevent_dsl
-{
-    template <typename Tag>
-    static constexpr _prevent<Token, Tag> error = _prevent<Token, Tag>{};
-};
-
-/// Requires that lookahead will match a rule at a location.
-template <typename Rule>
-LEXY_DEPRECATED_REQUIRE constexpr auto require(Rule rule)
-{
-    auto t = token(rule);
-    return _require_dsl<decltype(t)>{};
-}
-
-/// Requires that lookahead does not match a rule at a location.
-template <typename Rule>
-LEXY_DEPRECATED_REQUIRE constexpr auto prevent(Rule rule)
-{
-    auto t = token(rule);
-    return _prevent_dsl<decltype(t)>{};
-}
-
-template <typename Tag, typename Rule>
-LEXY_DEPRECATED_REQUIRE constexpr auto require(Rule rule)
-{
-    return require(rule).template error<Tag>;
-}
-template <typename Tag, typename Rule>
-LEXY_DEPRECATED_REQUIRE constexpr auto prevent(Rule rule)
-{
-    return prevent(rule).template error<Tag>;
 }
 } // namespace lexyd
 
