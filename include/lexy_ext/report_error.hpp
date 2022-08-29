8a317daaa10d930d18efbb1964f801abfc7fbda4
Jonathan Müller <git@foonathan.net>
2022-08-29 20:00:42 +0200

    Expose and extend `_detail::error_writer`

--- include/lexy_ext/report_error.hpp
+++ include/lexy_ext/report_error.hpp
@@ -10,26 +10,207 @@
 #include <lexy/input_location.hpp>
 #include <lexy/visualize.hpp>
 
-namespace lexy_ext::_detail
+namespace lexy_ext
 {
+/// The kind of diagnostic message.
+enum class diagnostic_kind
+{
+    error,
+    warning,
+    note,
+    info,
+    debug,
+    fixit,
+    help,
+};
+
+/// Classifies a source code annotation.
 enum class annotation_kind
 {
+    // foo
+    // ^^^ primary annotation
     primary,
+    // bar
+    // ~~~ secondary annotation
     secondary,
 };
 
+/// Formats and writes diagnostic messages.
 template <typename Input>
-struct error_writer
+class diagnostic_writer
 {
-    const Input*                input;
-    lexy::visualization_options opts;
+public:
+    explicit diagnostic_writer(const Input& input, lexy::visualization_options opts = {})
+    : _input(&input), _opts(opts)
+    {}
 
+    //=== writers ===//
+    /// Writes a message.
+    ///
+    /// error: A message.
+    template <typename OutputIt, typename Writer>
+    OutputIt write_message(OutputIt out, diagnostic_kind kind, const Writer& message)
+    {
+        using namespace lexy::_detail;
+        using lexy::_detail::color; // clang-cl bug
+
+        switch (kind)
+        {
+        case diagnostic_kind::error:
+            out = write_color<color::red, color::bold>(out, _opts);
+            out = write_str(out, "error: ");
+            break;
+        case diagnostic_kind::warning:
+            out = write_color<color::yellow, color::bold>(out, _opts);
+            out = write_str(out, " warn: ");
+            break;
+        case diagnostic_kind::note:
+            out = write_color<color::bold>(out, _opts);
+            out = write_str(out, " note: ");
+            break;
+        case diagnostic_kind::info:
+            out = write_color<color::bold>(out, _opts);
+            out = write_str(out, " info: ");
+            break;
+        case diagnostic_kind::debug:
+            out = write_color<color::bold>(out, _opts);
+            out = write_str(out, "debug: ");
+            break;
+        case diagnostic_kind::fixit:
+            out = write_color<color::bold>(out, _opts);
+            out = write_str(out, "fixit: ");
+            break;
+        case diagnostic_kind::help:
+            out = write_color<color::bold>(out, _opts);
+            out = write_str(out, " help: ");
+            break;
+        }
+        out = write_color<color::reset>(out, _opts);
+
+        out    = message(out, _opts);
+        *out++ = '\n';
+
+        return out;
+    }
+
+    /// Writes a path.
+    template <typename OutputIt>
+    OutputIt write_path(OutputIt out, const char* path) const
+    {
+        using namespace lexy::_detail;
+        using lexy::_detail::color; // clang-cl bug
+
+        out    = write_color<color::blue>(out, _opts);
+        out    = write_str(out, path);
+        out    = write_color<color::reset>(out, _opts);
+        *out++ = '\n';
+        return out;
+    }
+
+    /// Writes an empty annotation.
+    ///
+    ///    |\n
+    template <typename OutputIt>
+    OutputIt write_empty_annotation(OutputIt out) const
+    {
+        using namespace lexy::_detail;
+        using lexy::_detail::color; // clang-cl bug
+
+        out    = write_str(out, "     ");
+        out    = write_str(out, column());
+        *out++ = '\n';
+        return out;
+    }
+
+    /// Writes a highlighted line with an annotation.
+    ///
+    /// 100 | void foo();
+    ///     |      ^^^ annotation
+    template <typename OutputIt, typename Location, typename IteratorOrSize, typename Writer>
+    OutputIt write_annotation(OutputIt out, annotation_kind kind, const Location& begin_location,
+                              IteratorOrSize end, const Writer& message) const
+    {
+        using namespace lexy::_detail;
+        using lexy::_detail::color; // clang-cl bug
+
+        auto line = lexy::get_input_line_annotation(*_input, begin_location, end);
+        // If we had to truncate the annotation but don't include the newline,
+        // this is a "multiline" annotation in the last line.
+        auto annotate_eof = line.truncated_multiline && !line.annotated_newline;
+
+        //=== Line with file contents ===//
+        // Location column.
+        out    = write_color<color::blue>(out, _opts);
+        out    = write_format(out, "%4zd ", begin_location.line_nr());
+        out    = write_color<color::reset>(out, _opts);
+        out    = write_str(out, column());
+        *out++ = ' ';
+
+        // Print before underlined normally.
+        out = lexy::visualize_to(out, line.before, _opts);
+
+        // Print underlined colored.
+        out = colorize_underline(out, kind);
+        out = lexy::visualize_to(out, line.annotated, _opts.reset(lexy::visualize_use_color));
+        out = write_color<color::reset>(out, _opts);
+
+        // Print after underlined normally.
+        out    = lexy::visualize_to(out, line.after, _opts);
+        *out++ = '\n';
+
+        //=== Line with annotation ===//
+        // Initial column.
+        out    = write_str(out, "     ");
+        out    = write_str(out, column());
+        *out++ = ' ';
+
+        // Indent until the underline.
+        auto indent_count = lexy::visualization_display_width(line.before, _opts);
+        for (auto i = 0u; i != indent_count; ++i)
+            *out++ = ' ';
+
+        // Colorize.
+        out = colorize_underline(out, kind);
+
+        // Then underline.
+        auto underline_count = lexy::visualization_display_width(line.annotated, _opts);
+        for (auto i = 0u; i != underline_count; ++i)
+            out = write_str(out, underline(kind));
+        if (underline_count == 0 || annotate_eof)
+            out = write_str(out, underline(kind));
+        *out++ = ' ';
+
+        // Print the message.
+        out    = message(out, _opts.reset(lexy::visualize_use_color));
+        *out++ = '\n';
+
+        return write_color<color::reset>(out, _opts);
+    }
+
+private:
     const auto* column() const
     {
-        if (opts.is_set(lexy::visualize_use_unicode))
+        if (_opts.is_set(lexy::visualize_use_unicode))
             return u8"│";
         else
             return u8"|";
+    }
+
+    template <typename OutputIt>
+    OutputIt colorize_underline(OutputIt out, annotation_kind kind) const
+    {
+        using namespace lexy::_detail;
+        using lexy::_detail::color; // clang-cl bug
+
+        switch (kind)
+        {
+        case annotation_kind::primary:
+            return write_color<color::red, color::bold>(out, _opts);
+        case annotation_kind::secondary:
+            return write_color<color::yellow>(out, _opts);
+        }
+
+        return out;
     }
 
     const auto* underline(annotation_kind kind) const
@@ -45,106 +226,10 @@ struct error_writer
         return "";
     }
 
-    //=== writers ===//
-    template <typename OutputIt, typename Writer>
-    OutputIt write_message(OutputIt out, const Writer& message)
-    {
-        using namespace lexy::_detail;
-        using lexy::_detail::color; // clang-cl bug
-
-        out    = write_color<color::red, color::bold>(out, opts);
-        out    = write_str(out, "error: ");
-        out    = write_color<color::reset>(out, opts);
-        out    = message(out, opts);
-        *out++ = '\n';
-
-        return out;
-    }
-
-    template <typename OutputIt>
-    OutputIt write_empty_annotation(OutputIt out) const
-    {
-        using namespace lexy::_detail;
-        using lexy::_detail::color; // clang-cl bug
-
-        out    = write_str(out, "     ");
-        out    = write_str(out, column());
-        *out++ = '\n';
-        return out;
-    }
-
-    template <typename OutputIt, typename Location, typename IteratorOrSize, typename Writer>
-    OutputIt write_annotation(OutputIt out, annotation_kind kind, const Location& begin_location,
-                              IteratorOrSize end, const Writer& message) const
-    {
-        using namespace lexy::_detail;
-        using lexy::_detail::color; // clang-cl bug
-
-        auto colorize_underline = [&](OutputIt out) {
-            switch (kind)
-            {
-            case annotation_kind::primary:
-                return write_color<color::red, color::bold>(out, opts);
-            case annotation_kind::secondary:
-                return write_color<color::yellow>(out, opts);
-            }
-
-            return out;
-        };
-
-        auto line = lexy::get_input_line_annotation(*input, begin_location, end);
-        // If we had to truncate the annotation but don't include the newline,
-        // this is a "multiline" annotation in the last line.
-        auto annotate_eof = line.truncated_multiline && !line.annotated_newline;
-
-        //=== Line with file contents ===//
-        // Location column.
-        out    = write_format(out, "%4zd ", begin_location.line_nr());
-        out    = write_str(out, column());
-        *out++ = ' ';
-
-        // Print before underlined normally.
-        out = lexy::visualize_to(out, line.before, opts);
-
-        // Print underlined colored.
-        out = colorize_underline(out);
-        out = lexy::visualize_to(out, line.annotated, opts.reset(lexy::visualize_use_color));
-        out = write_color<color::reset>(out, opts);
-
-        // Print after underlined normally.
-        out    = lexy::visualize_to(out, line.after, opts);
-        *out++ = '\n';
-
-        //=== Line with annotation ===//
-        // Initial column.
-        out    = write_str(out, "     ");
-        out    = write_str(out, column());
-        *out++ = ' ';
-
-        // Indent until the underline.
-        auto indent_count = lexy::visualization_display_width(line.before, opts);
-        for (auto i = 0u; i != indent_count; ++i)
-            *out++ = ' ';
-
-        // Colorize.
-        out = colorize_underline(out);
-
-        // Then underline.
-        auto underline_count = lexy::visualization_display_width(line.annotated, opts);
-        for (auto i = 0u; i != underline_count; ++i)
-            out = write_str(out, underline(kind));
-        if (underline_count == 0 || annotate_eof)
-            out = write_str(out, underline(kind));
-        *out++ = ' ';
-
-        // Print the message.
-        out    = message(out, opts.reset(lexy::visualize_use_color));
-        *out++ = '\n';
-
-        return write_color<color::reset>(out, opts);
-    }
+    const Input*                _input;
+    lexy::visualization_options _opts;
 };
-} // namespace lexy_ext::_detail
+} // namespace lexy_ext
 
 namespace lexy_ext::_detail
 {
@@ -152,7 +237,7 @@ template <typename OutputIt, typename Production, typename Input, typename Reade
 OutputIt write_error(OutputIt out, const lexy::error_context<Production, Input>& context,
                      const lexy::error<Reader, Tag>& error, lexy::visualization_options opts)
 {
-    _detail::error_writer<Input> writer{&context.input(), opts};
+    diagnostic_writer<Input> writer(context.input(), opts);
 
     // Convert the context location and error location into line/column information.
     auto context_location = lexy::get_input_location(context.input(), context.position());
@@ -160,11 +245,12 @@ OutputIt write_error(OutputIt out, const lexy::error_context<Production, Input>&
         = lexy::get_input_location(context.input(), error.position(), context_location.anchor());
 
     // Write the main error headline.
-    out = writer.write_message(out, [&](OutputIt out, lexy::visualization_options) {
-        out = lexy::_detail::write_str(out, "while parsing ");
-        out = lexy::_detail::write_str(out, context.production());
-        return out;
-    });
+    out = writer.write_message(out, diagnostic_kind::error,
+                               [&](OutputIt out, lexy::visualization_options) {
+                                   out = lexy::_detail::write_str(out, "while parsing ");
+                                   out = lexy::_detail::write_str(out, context.production());
+                                   return out;
+                               });
     out = writer.write_empty_annotation(out);
 
     // Write an annotation for the context.
@@ -252,14 +338,13 @@ struct _report_error
             return _count;
         }
     };
-
     constexpr auto sink() const
     {
         return _sink{};
     }
 };
 
-// The error callback that prints to stderr.
+/// An error callback that uses diagnostic_writer to print to stderr (by default).
 constexpr auto report_error = _report_error{};
 } // namespace lexy_ext
 
