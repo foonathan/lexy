011e1a6ae808fa770445a7ccd0b1cdf4b1a8d9f7
foonathan <foonathan@users.noreply.github.com>
2022-12-07 09:44:23 +0000

    deploy: af3a30aebf8e6f9618285b3e6370325ba9273c43

--- cpp/playground_headers.single.hpp
+++ cpp/playground_headers.single.hpp
@@ -4272,6 +4272,19 @@ struct _tokk : token_base<_tokk<Kind, Token>, Token>
 template <typename Tag, typename Token>
 struct _toke : token_base<_toke<Tag, Token>, Token>
 {
+    // If we're overriding the error for a char class rule, we also want to change its error
+    // reporting. Otherwise, rules such as `dsl::delimited()` building on char classes will generate
+    // the "wrong" error.
+    //
+    // If it's not a char class, adding this function doesn't hurt.
+    template <typename Reader, typename Context>
+    static constexpr void char_class_report_error(Context&                  context,
+                                                  typename Reader::iterator position)
+    {
+        auto err = lexy::error<Reader, Tag>(position, position);
+        context.on(_ev::error{}, err);
+    }
+
     template <typename Reader>
     struct tp : lexy::token_parser_for<Token, Reader>
     {
@@ -12257,6 +12270,7 @@ constexpr auto try_(Rule, Recover)
 
 
 
+
 // Copyright (C) 2020-2022 Jonathan Müller and lexy contributors
 // SPDX-License-Identifier: BSL-1.0
 
@@ -12471,6 +12485,15 @@ ws_handler(Context& context) -> ws_handler<typename Context::handler_type>;
 template <typename>
 using ws_result = bool;
 
+template <typename WhitespaceRule>
+constexpr bool space_is_definitely_whitespace()
+{
+    if constexpr (lexy::is_char_class_rule<WhitespaceRule>)
+        return WhitespaceRule::char_class_ascii().contains[int(' ')];
+    else
+        return false;
+}
+
 template <typename WhitespaceRule, typename Handler, typename Reader>
 constexpr auto skip_whitespace(ws_handler<Handler>&& handler, Reader& reader)
 {
@@ -12479,8 +12502,29 @@ constexpr auto skip_whitespace(ws_handler<Handler>&& handler, Reader& reader)
     if constexpr (lexy::is_token_rule<WhitespaceRule>)
     {
         // Parsing a token repeatedly cannot fail, so we can optimize it.
-        while (lexy::try_match_token(WhitespaceRule{}, reader))
-        {}
+
+        if constexpr (_detail::is_swar_reader<Reader> //
+                      && space_is_definitely_whitespace<WhitespaceRule>())
+        {
+            while (true)
+            {
+                // Skip as many spaces as possible.
+                using char_type = typename Reader::encoding::char_type;
+                while (reader.peek_swar() == _detail::swar_fill(char_type(' ')))
+                    reader.bump_swar();
+
+                // We no longer have a space, skip the entire whitespace rule once.
+                if (!lexy::try_match_token(WhitespaceRule{}, reader))
+                    // If that fails, we definitely have no more whitespace.
+                    break;
+            }
+        }
+        else
+        {
+            // Without SWAR, we just repeatedly skip the whitespace rule.
+            while (lexy::try_match_token(WhitespaceRule{}, reader))
+            {}
+        }
 
         handler.real_on(lexy::parse_events::token{}, lexy::whitespace_token_kind, begin,
                         reader.position());
@@ -15519,7 +15563,8 @@ struct _del : rule_base
                                       _del_limit<Limit>, Limit>;
 
     template <typename CloseParser, typename Context, typename Reader, typename Sink>
-    static constexpr bool _loop(CloseParser& close, Context& context, Reader& reader, Sink& sink)
+    LEXY_PARSER_FUNC static bool _loop(CloseParser& close, Context& context, Reader& reader,
+                                       Sink& sink)
     {
         auto                     del_begin = reader.position();
         _del_chars<Char, Reader> cur_chars(reader);
@@ -18021,19 +18066,26 @@ constexpr bool _ndigits_can_overflow()
     return N >= max_digit_count;
 }
 
-// Parses T in the Base while checking for overflow.
+// Parses T in the Base without checking for overflow.
 template <typename T, typename Base>
 struct _unbounded_integer_parser
 {
-    using traits      = lexy::integer_traits<T>;
-    using result_type = typename traits::type;
-    using base        = Base;
+    using traits = lexy::integer_traits<T>;
+    using base   = Base;
 
     static constexpr auto radix = Base::digit_radix;
 
-    template <typename Iterator>
-    static constexpr bool parse(result_type& result, Iterator cur, Iterator end)
+    struct result_type
     {
+        typename traits::type value;
+        std::false_type       overflow;
+    };
+
+    template <typename Iterator>
+    static constexpr result_type parse(Iterator cur, Iterator end)
+    {
+        typename traits::type value(0);
+
         // Just parse digits until we've run out of digits.
         while (cur != end)
         {
@@ -18042,94 +18094,83 @@ struct _unbounded_integer_parser
                 // Skip digit separator.
                 continue;
 
-            traits::template add_digit_unchecked<radix>(result, digit);
+            traits::template add_digit_unchecked<radix>(value, digit);
         }
 
-        return true;
+        return {value, {}};
     }
 };
 
-// Parses T in the Base without checking for overflow.
+// Parses T in the Base while checking for overflow.
 template <typename T, typename Base, bool AssumeOnlyDigits>
 struct _bounded_integer_parser
 {
-    using traits      = lexy::integer_traits<T>;
-    using result_type = typename traits::type;
-    using base        = Base;
+    using traits = lexy::integer_traits<T>;
+    using base   = Base;
 
-    static constexpr auto radix = Base::digit_radix;
+    static constexpr auto radix           = Base::digit_radix;
+    static constexpr auto max_digit_count = traits::template max_digit_count<radix>;
+    static_assert(max_digit_count > 1, "integer must be able to store all possible digit values");
+
+    struct result_type
+    {
+        typename traits::type value;
+        bool                  overflow;
+    };
 
     template <typename Iterator>
-    static constexpr unsigned find_digit(Iterator& cur, Iterator end)
+    static constexpr result_type parse(Iterator cur, Iterator end)
     {
-        if constexpr (AssumeOnlyDigits)
+        // Find the first non-zero digit.
+        // Note that we always need a loop, even if leading zeros are not allowed:
+        // error recovery might get them anyway.
+        auto first_digit = 0u;
+        while (true)
         {
             if (cur == end)
-                return unsigned(-1);
-            else
-                return Base::digit_value(*cur++);
+                return {typename traits::type(0), false};
+
+            first_digit = Base::digit_value(*cur++);
+            if (first_digit != 0 && first_digit < radix)
+                break;
         }
-        else
+
+        // At this point, we've parsed exactly one non-zero digit, so we can assign.
+        auto value = typename traits::type(first_digit);
+
+        // Handle at most the number of remaining digits.
+        // Due to the fixed loop count, it is most likely unrolled.
+        for (std::size_t digit_count = 1; digit_count < max_digit_count; ++digit_count)
         {
+            // Find the next digit.
             auto digit = 0u;
             do
             {
                 if (cur == end)
-                    return unsigned(-1);
+                    return {value, false};
 
                 digit = Base::digit_value(*cur++);
-            } while (digit >= Base::digit_radix);
-            return digit;
-        }
-    }
+                // If we can assume it's a digit, we don't need the comparison.
+            } while (AssumeOnlyDigits ? false : digit >= Base::digit_radix);
 
-    template <typename Iterator>
-    static constexpr bool parse(result_type& result, Iterator cur, Iterator end)
-    {
-        constexpr auto max_digit_count = traits::template max_digit_count<radix>;
-        static_assert(max_digit_count > 1,
-                      "integer must be able to store all possible digit values");
-
-        // Skip leading zeroes.
-        while (true)
-        {
-            if (cur == end)
-                return true; // We only had zeroes.
-
-            const auto digit = Base::digit_value(*cur++);
-            if (digit == 0 || digit >= radix)
-                continue; // Zero or digit separator.
-
-            // First non-zero digit, so we can assign it instead of adding.
-            result = result_type(digit);
-            break;
-        }
-        // At this point, we've parsed exactly one non-zero digit.
-
-        // Handle max_digit_count - 1 digits without checking for overflow.
-        // We cannot overflow, as the maximal value has one digit more.
-        for (std::size_t digit_count = 1; digit_count < max_digit_count - 1; ++digit_count)
-        {
-            auto digit = find_digit(cur, end);
-            if (digit == unsigned(-1))
-                return true;
-
-            traits::template add_digit_unchecked<radix>(result, digit);
-        }
-
-        // Handle the final digit, if there is any, while checking for overflow.
-        {
-            auto digit = find_digit(cur, end);
-            if (digit == unsigned(-1))
-                return true;
-
-            if (!traits::template add_digit_checked<radix>(result, digit))
-                return false;
+            // We need to handle the last loop iteration special.
+            // (The compiler will not generate a branch here.)
+            if (digit_count == max_digit_count - 1)
+            {
+                // The last digit might overflow, so check for it.
+                if (!traits::template add_digit_checked<radix>(value, digit))
+                    return {value, true};
+            }
+            else
+            {
+                // Add the digit without checking as it can't overflow.
+                traits::template add_digit_unchecked<radix>(value, digit);
+            }
         }
 
         // If we've reached this point, we've parsed the maximal number of digits allowed.
-        // Now we can only fail if there are still digits left.
-        return cur == end;
+        // Now we can only overflow if there are still digits left.
+        return {value, cur != end};
     }
 };
 template <typename T, typename Base, bool AssumeOnlyDigits>
@@ -18147,7 +18188,7 @@ struct _integer_parser_digits<T, _digits<Base>>
 template <typename T, typename Base>
 struct _integer_parser_digits<T, _digits_t<Base>>
 {
-    using type = _integer_parser<T, Base, false>;
+    using type = _integer_parser<T, Base, true>;
 };
 template <typename T, typename Base, typename Sep>
 struct _integer_parser_digits<T, _digits_s<Base, Sep>>
@@ -18188,8 +18229,8 @@ struct _int : _copy_base<Token>
                                            typename Reader::iterator begin,
                                            typename Reader::iterator end, Args&&... args)
         {
-            auto result = typename IntParser::result_type(0);
-            if (!IntParser::parse(result, begin, end))
+            auto [value, overflow] = IntParser::parse(begin, end);
+            if (overflow)
             {
                 // Raise error but recover.
                 using tag = lexy::_detail::type_or<Tag, lexy::integer_overflow>;
@@ -18198,7 +18239,7 @@ struct _int : _copy_base<Token>
 
             // Need to skip whitespace now as well.
             return lexy::whitespace_parser<Context, NextParser>::parse(context, reader,
-                                                                       LEXY_FWD(args)..., result);
+                                                                       LEXY_FWD(args)..., value);
         }
     };
 
@@ -18276,7 +18317,7 @@ struct _int : _copy_base<Token>
 
 template <typename T, typename Base>
 struct _int_dsl : _int<_digits<lexy::_detail::type_or<Base, decimal>>,
-                       _integer_parser<T, lexy::_detail::type_or<Base, decimal>, true>, void>
+                       _integer_parser_for<T, _digits<lexy::_detail::type_or<Base, decimal>>>, void>
 {
     template <typename Digits>
     constexpr auto operator()(Digits) const
@@ -18626,7 +18667,7 @@ template <typename Item, typename Sep>
 struct _lst : _copy_base<Item>
 {
     template <typename Context, typename Reader, typename Sink>
-    static constexpr bool _loop(Context& context, Reader& reader, Sink& sink)
+    LEXY_PARSER_FUNC static bool _loop(Context& context, Reader& reader, Sink& sink)
     {
         while (true)
         {
@@ -18796,8 +18837,8 @@ struct _lstt : rule_base
     };
 
     template <typename TermParser, typename Context, typename Reader, typename Sink>
-    static constexpr bool _loop(_state initial_state, TermParser& term, Context& context,
-                                Reader& reader, Sink& sink)
+    LEXY_PARSER_FUNC static bool _loop(_state initial_state, TermParser& term, Context& context,
+                                       Reader& reader, Sink& sink)
     {
         auto state = initial_state;
 
@@ -21042,6 +21083,30 @@ constexpr auto twice(Rule rule, Sep sep)
 
 namespace lexyd::unicode
 {
+struct _nctrl : char_class_base<_nctrl>
+{
+    static LEXY_CONSTEVAL auto char_class_name()
+    {
+        return "code-point.non-control";
+    }
+
+    static LEXY_CONSTEVAL auto char_class_ascii()
+    {
+        return ascii::_print::char_class_ascii();
+    }
+
+    static constexpr bool char_class_match_cp(char32_t cp)
+    {
+        return !lexy::code_point(cp).is_control();
+    }
+
+    template <typename Encoding>
+    static constexpr auto char_class_match_swar(lexy::_detail::swar_int c)
+    {
+        return ascii::_print::template char_class_match_swar<Encoding>(c);
+    }
+};
+
 struct _control : char_class_base<_control>
 {
     static LEXY_CONSTEVAL auto char_class_name()
@@ -21066,6 +21131,15 @@ struct _control : char_class_base<_control>
     }
 };
 inline constexpr auto control = _control{};
+
+constexpr auto operator-(_control)
+{
+    return _nctrl{};
+}
+constexpr auto operator-(_nctrl)
+{
+    return _control{};
+}
 
 //=== whitespace ===//
 struct _blank : char_class_base<_blank>
