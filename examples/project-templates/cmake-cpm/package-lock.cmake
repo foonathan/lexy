9ad853f26e5a35003c3e0c317a15dc632c1dfd7b
Nicholas Sielicki <opensource@nslick.com>
2025-06-24 16:26:36 -0700

    examples: add CPM.cmake example project
    Signed-off-by: Nicholas Sielicki <opensource@nslick.com>

---
+++ examples/project-templates/cmake-cpm/package-lock.cmake
@@ -0,0 +1,17 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# lexy (unversioned)
+# CPMDeclarePackage(lexy
+#  NAME lexy
+#  GIT_TAG 20926cf89aaccafc0fa44e02de14fdc411493822
+#  GITHUB_REPOSITORY foonathan/lexy
+#  OPTIONS
+#    "LEXY_FORCE_CPP17      OFF"
+#    "LEXY_BUILD_BENCHMARKS OFF"
+#    "LEXY_BUILD_EXAMPLES   OFF"
+#    "LEXY_BUILD_TESTS      OFF"
+#    "LEXY_BUILD_DOCS       OFF"
+#    "LEXY_BUILD_PACKAGE    OFF"
+#    "LEXY_ENABLE_INSTALL   ON"
+#)
